2e446679f1c91bd8381732e5b797d3e6d1a5b6a6
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-11 21:47:19 +0200

    Implemented basic credits menu
    The credits menu now has a button to return to the main menu and displays text with the game credits.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/CreditsMenuController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsMenuController : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void ReturnToMenu()
+    {
+        SceneLoadingManager.Instance?.LoadSceneMainMenu();
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
