6c5a7538d7280a93d48c47967dc63fa13603564b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-11 17:22:06 +0200

    Added debug code running function
    Altough the caller can use GetDebugEnabled() to check if the debug is enabled to run debug code, a simple function has been provided to be able to more quickly write and execute debug code with lambdas and such.

--- UnityGame/GBS/Assets/Scripts/Managers/DebugManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/DebugManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,7 +27,14 @@ public class DebugManager : Singleton<DebugManager>
 
     public void Log(string str)
     {
-        Debug.Log($"[Debug] : {str}");
+        if(this.debugEnabled)
+            Debug.Log($"[Debug] : {str}");
+    }
+
+    public void Run(Action function)
+    {
+        if(this.debugEnabled)
+            function?.Invoke();
     }
 
     public void SetDebugEnabled(bool value)
