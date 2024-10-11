71a005f4b714eb72052be977cd475ea95d69fb57
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-11 18:53:17 +0200

    Implemented FPS display
    The FPS are now displayed on the screen using the InfoUI.
    
    The InfoUI prefab contains the entire setup for the UI to display information. As of now, it only displays the framerate of the program.
    
    The InfoUIController class controls the logic to update the framerate every so often.
    
    The fps display text is not updated every single frame to prevent wasting resources.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/InfoUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/InfoUIController.cs
@@ -7,18 +7,60 @@ public class InfoUIController : UIController
 {
     #region Variables
 
-    // [Header("DebugUIController Components")]
+    [Header("Info UI Components")]
+    [SerializeField] private TMP_Text fpsText;
+
+    [Header("Info UI Configuration")]
+    [SerializeField] private float fpsUpdateTime = 0.4f;
+
+    private float accumulatedTime = 0.0f;
+    private int accumulatedFrames = 0;
+
+    private float fps = 0.0f;
 
     #endregion
-    // Start is called before the first frame update
+
+    #region MonoBehaviour
+
     void Start()
     {
-        
+        // Rough initial estimate of the FPS on the first frame of the game.
+        this.UpdateFpsDisplayText((int)(1.0f / Time.deltaTime));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        this.UpdateFps(Time.deltaTime);
     }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+
+    private void UpdateFps(float delta)
+    {
+        this.accumulatedFrames += 1;
+        this.accumulatedTime += delta;
+
+        if (this.accumulatedTime < this.fpsUpdateTime)
+            return;
+
+        this.fps = this.accumulatedFrames / this.accumulatedTime;
+        this.UpdateFpsDisplayText((int)fps);
+
+        this.accumulatedFrames = 0;
+        this.accumulatedTime = 0.0f;
+    }
+
+    private void UpdateFpsDisplayText(int fps)
+    {
+        if (this.fpsText == null)
+            return;
+        this.fpsText.text = $"{fps} FPS";
+    }
+
+    #endregion
 }
