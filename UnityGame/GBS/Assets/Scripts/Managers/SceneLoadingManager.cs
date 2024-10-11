096ff8b936520cecb3448024854b5230cc26fbc3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-11 22:03:11 +0200

    Added funky comment to SceneLoadingManager
    funky

--- UnityGame/GBS/Assets/Scripts/Managers/SceneLoadingManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/SceneLoadingManager.cs
@@ -32,6 +32,11 @@ public class SceneLoadingManager : SingletonPersistent<SceneLoadingManager>
     public void LoadScene(string name)
     {
         SceneManager.LoadScene(name, LoadSceneMode.Single);
+        // The call to SetActiveScene requires that the input scene we're passing is registered as loaded.
+        // For that to happen, Unity requires at least 1 frame between the LoadScene() call and the SetActiveScene() call.
+        // This would require making use of a coroutine or some waiting mechanism to delay the calls by 1 update / frame.
+        // This call is now disabled because it is not really needed as of now, as it would only make sense if we were to load multiple scenes at a time,
+        // but we're only using LoadSceneMode.Single so fuck it.
         // SceneManager.SetActiveScene(SceneManager.GetSceneByName(name));
     }
 
