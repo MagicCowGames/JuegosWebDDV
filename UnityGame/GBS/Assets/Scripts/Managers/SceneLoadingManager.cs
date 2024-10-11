53b7f4e36af26e133f52f3312dd8e019e5570191
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-11 21:45:09 +0200

    Created SceneLoadingManager
    A manager class in charge of encapsulating scene loading logic.
    
    Will make it easier to load scenes in MP if MP support is added in the future.

---
+++ UnityGame/GBS/Assets/Scripts/Managers/SceneLoadingManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadingManager : SingletonPersistent<SceneLoadingManager>
+{
+    #region Variables
+
+    [Header("Scene Names - Menus")]
+    [SerializeField] private string mainMenuScene;
+    [SerializeField] private string creditsScene;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(name));
+    }
+
+    #endregion
+
+    #region PublicMethods Specific Scenes
+
+    public void LoadSceneMainMenu()
+    {
+        LoadScene(mainMenuScene);
+    }
+
+    public void LoadSceneCredits()
+    {
+        LoadScene(creditsScene);
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
