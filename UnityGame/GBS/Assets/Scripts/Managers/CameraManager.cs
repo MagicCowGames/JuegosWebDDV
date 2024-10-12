9b535d23a4335eb4c938dbccb2865108338bb3d4
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-12 15:20:09 +0200

    Improved camera manager
    The camera manager is now a Singleton<> and implements the IManager interface.
    
    The camera rotation is now updated as well to match the target transform.
    
    The camera manager now makes sure that the active camera and target are not null before updating the camera position and rotation.
    
    TODO : The loading manager has to call ResetManager() on camera manager to make sure that we don't have dangling references to objects that no longer exist on the scene when loading a new scene.

--- UnityGame/GBS/Assets/Scripts/Managers/CameraManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/CameraManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+public class CameraManager : Singleton<CameraManager>, IManager
 {
     #region Variables
 
@@ -27,13 +27,43 @@ public class CameraManager : MonoBehaviour
     #endregion
 
     #region PublicMethods
+
+    public void SetActiveTarget(Transform target)
+    {
+        this.activeTarget = target;
+    }
+
+    public void SetActiveCamera(Camera camera)
+    {
+        this.activeCamera = camera;
+    }
+
+    public void ResetManager()
+    {
+        SetActiveTarget(null);
+        SetActiveCamera(null);
+    }
+
     #endregion
 
     #region PrivateMethods
 
     private void UpdateCamera(float delta)
     {
+        if (this.activeCamera == null || this.activeTarget == null)
+            return;
+        UpdateCameraPosition(delta);
+        UpdateCameraRotation(delta);
+    }
+
+    private void UpdateCameraPosition(float delta)
+    {
         this.activeCamera.transform.position = Vector3.Lerp(this.activeCamera.transform.position, this.activeTarget.position, Mathf.Min(delta * this.speed, 1.0f));
+    }
+
+    private void UpdateCameraRotation(float delta)
+    {
+        this.activeCamera.transform.rotation = Quaternion.Lerp(this.activeCamera.transform.rotation, this.activeTarget.rotation, Mathf.Min(delta * this.speed, 1.0f));
     }
 
     #endregion
