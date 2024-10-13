2dea99d75b971b90e5b1098c386229851b224a00
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 02:17:42 +0200

    Added pause blocking mechanism
    There is now a CanPause property that determines whether we can pause or not. If pausing is not allowed, then we can handle the escape key some other way.

--- UnityGame/GBS/Assets/Scripts/Utility/GameUtility.cs
+++ UnityGame/GBS/Assets/Scripts/Utility/GameUtility.cs
@@ -8,6 +8,7 @@ public static class GameUtility
     #region Pause
 
     private static bool isPaused = false;
+    private static bool canPause = true;
 
     public static bool GetPaused()
     {
@@ -16,6 +17,13 @@ public static class GameUtility
 
     public static void SetPaused(bool value)
     {
+        // If we try to pause but we can't pause cause we ain't allowed to do so at this moment, then just get the fuck out with an early return and get it over with.
+        if (!canPause && value)
+        {
+            DebugManager.Instance?.Log("Cannot pause at this moment!");
+            return;
+        }
+
         isPaused = value;
 
         float timeScale;
@@ -53,6 +61,19 @@ public static class GameUtility
     public static void Resume()
     {
         SetPaused(false);
+    }
+
+    // This should be used any time the user enters a menu we don't want them to be able to pause in.
+    // This will be useful if, for example, we want the escape key on those menus to mean "get out of this menu".
+    // Also on the death screen, but yeah.
+    public static void SetCanPause(bool value)
+    {
+        canPause = value;
+    }
+
+    public static bool GetCanPause()
+    {
+        return canPause;
     }
 
     #endregion
