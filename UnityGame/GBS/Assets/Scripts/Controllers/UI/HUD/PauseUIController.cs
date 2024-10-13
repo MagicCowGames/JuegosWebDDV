c144d73aab948bd4483de888157f238cd2543579
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 02:24:36 +0200

    Fixed PauseUIController by making it inherit UIController
    There was a typo that made it so that the pause ui controller would inherit from monobehaviour rather than UIController

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PauseUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PauseUIController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseUIController : MonoBehaviour
+public class PauseUIController : UIController
 {
     #region Variables
     #endregion
