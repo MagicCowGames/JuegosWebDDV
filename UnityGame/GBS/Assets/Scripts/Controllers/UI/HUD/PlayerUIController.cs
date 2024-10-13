d27f01329591b9e2b258e04640c7571084f3686e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 23:12:37 +0200

    Added code for element display updating to player ui

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PlayerUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PlayerUIController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUIController : UIController
 {
     #region Variables
+
+    [Header("Player UI Controller Components")]
+    [SerializeField] private Image[] elementQueueImages;
+
     #endregion
 
     #region MonoBehaviour
@@ -33,6 +38,22 @@ public class PlayerUIController : UIController
     public void Button_AddElement_Death() { Button_AddElement(Element.Death); }
     public void Button_AddElement_Earth() { Button_AddElement(Element.Earth); }
     public void Button_AddElement_Fire() { Button_AddElement(Element.Fire); }
+
+    public void UpdateElementDisplay(ElementQueue queue)
+    {
+        // This should never happen, but if it does, we're fucked, so just return early to prevent any issues.
+        if (ElementManager.Instance == null)
+            return;
+
+        // Reset the entire queue to the default sprite / Element.None sprite
+        for (int i = 0; i < this.elementQueueImages.Length; ++i)
+            this.elementQueueImages[i].sprite = ElementManager.Instance.GetSprite(Element.None);
+
+        // Replace the sprites for those of the elements within the queue
+        var elements = queue.Elements;
+        for (int i = 0; i < queue.Count; ++i)
+            this.elementQueueImages[i].sprite = ElementManager.Instance.GetSprite(elements[i]);
+    }
 
     #endregion
 
