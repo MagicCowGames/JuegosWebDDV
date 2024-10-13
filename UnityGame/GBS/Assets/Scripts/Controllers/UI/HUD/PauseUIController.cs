d652399537b077f935c981c5e40f9c8f98c8fce9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 02:08:56 +0200

    Created pause UI controller
    The controller makes use of the game utility functionality to resume the game.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PauseUIController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUIController : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void Button_Resume()
+    {
+        GameUtility.Resume();
+    }
+
+    public void Button_ReturnToMenu()
+    {
+        GameUtility.Resume(); // Resume before loading main menu
+        SceneLoadingManager.Instance?.LoadSceneMainMenu();
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
