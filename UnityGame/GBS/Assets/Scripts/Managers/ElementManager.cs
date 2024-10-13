3735c8b3b585d9220f04b1432a35b138e899017c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 17:43:22 +0200

    Created basic structure for Element Manager
    This manager class will be in charge of managing the game's magic elemental system.
    
    This will serve as a service provider for handling elemental combinations and other such operations.

---
+++ UnityGame/GBS/Assets/Scripts/Managers/ElementManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementManager : SingletonPersistent<ElementManager>
+{
+    #region Structs
+    #endregion
+
+    #region Variables
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
