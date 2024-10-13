92a5271a7987cbde2a40b40634cf4251ab892a0c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 20:29:28 +0200

    Added functions to queue elements from UI buttons
    These functions will be called when using the UI element buttons on mobile devices.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PlayerUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PlayerUIController.cs
@@ -22,8 +22,23 @@ public class PlayerUIController : UIController
     #endregion
 
     #region PublicMethods
+
+    public void Button_AddElement_Water() { Button_AddElement(Element.Water); }
+    public void Button_AddElement_Heal() { Button_AddElement(Element.Heal); }
+    public void Button_AddElement_Shield() { Button_AddElement(Element.Shield); }
+    public void Button_AddElement_Cold() { Button_AddElement(Element.Cold); }
+    public void Button_AddElement_Electricity() { Button_AddElement(Element.Electricity); }
+    public void Button_AddElement_Death() { Button_AddElement(Element.Death); }
+    public void Button_AddElement_Fire() { Button_AddElement(Element.Fire); }
+
     #endregion
 
     #region PrivateMethods
+
+    private void Button_AddElement(Element element)
+    {
+        InputManager.Instance?.AddElement(element);
+    }
+
     #endregion
 }
