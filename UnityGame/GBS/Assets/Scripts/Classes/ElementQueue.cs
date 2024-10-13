30bb72d511c01c514a349f9f03b86b598af3eb98
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 21:43:52 +0200

    Added variables to element queue class

--- UnityGame/GBS/Assets/Scripts/Classes/ElementQueue.cs
+++ UnityGame/GBS/Assets/Scripts/Classes/ElementQueue.cs
@@ -8,6 +8,11 @@ public class ElementQueue
     #endregion
 
     #region Constructor
+
+    public Element[] Elements { get; private set; } // list of elements
+    public int Count { get; private set; } // number of elements within the list
+    public int Slots { get; private set; } // number of element slots (actual length of the array)
+
     #endregion
 
     #region PublicMethods
