3e2f900853411e13f56949ab83881b6ffc94c9aa
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 22:35:16 +0200

    Added counter array for each element type to queue
    The element queue now holds a variable that is an array of integers, each position holding the number of times a certain element is present within the element queue.

--- UnityGame/GBS/Assets/Scripts/Classes/ElementQueue.cs
+++ UnityGame/GBS/Assets/Scripts/Classes/ElementQueue.cs
@@ -6,9 +6,13 @@ public class ElementQueue
 {
     #region Variables
 
+    // Primary variables
     public Element[] Elements { get; private set; } // list of elements
     public int Count { get; private set; } // number of elements within the list
     public int Slots { get; private set; } // number of element slots (actual length of the array)
+
+    // Secondary variables
+    public int[] ElementsCounts { get; private set; } // holds a count of the number of elements of a given type that is stored within the queue. Can be used to check what spell type to generate out of the current element queue.
 
     #endregion
 
@@ -21,6 +25,10 @@ public class ElementQueue
         for (int i = 0; i < this.Slots; ++i)
             this.Elements[i] = Element.None;
         this.Count = 0;
+
+        this.ElementsCounts = new int[(int)Element.COUNT];
+        for (int i = 0; i < this.ElementsCounts.Length; ++i)
+            this.ElementsCounts[i] = 0;
     }
 
     #endregion
@@ -32,6 +40,9 @@ public class ElementQueue
         for (int i = 0; i < this.Slots; ++i)
             this.Elements[i] = Element.None;
         this.Count = 0;
+
+        for (int i = 0; i < this.ElementsCounts.Length; ++i)
+            this.ElementsCounts[i] = 0;
     }
 
     public void Add(Element element)
@@ -49,17 +60,18 @@ public class ElementQueue
         if (i < 0 || i >= this.Count)
             return;
 
-        // Remove the element.
+        // Remove the element (only need to reduce counts)
         // We only need to decrease the count by one, because we're going to shift the elements back and then set the last to None.
         // In short, we don't need to set Elements[i] to None yet.
-        this.Count -= 1;
+        this.Count -= 1; // Reduce the total element count.
+        this.ElementsCounts[(int)this.Elements[i]] -= 1; // Reduce the count for this specific element.
 
         // Shift elements to the left, leaving the None to the right most possible slot
         for (int j = i; j < this.Count - 1; ++j)
             this.Elements[j] = this.Elements[j + 1];
 
         // Set the last element to None
-        this.Elements[this.Count] = Element.None;
+        this.Elements[this.Count] = Element.None; // Equivalent of pushing the Elements[i] being None to the front of the array, only that we skip the stupid temp var bullshit that would be needed if we did it that way...
     }
 
     #endregion
