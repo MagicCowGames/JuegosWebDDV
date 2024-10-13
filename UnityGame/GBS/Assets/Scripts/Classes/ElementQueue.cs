4eaa9c5cbec103da759aede7dba2d65bb1a6944a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 20:26:47 +0200

    Created ElementQueue class
    This class acts as a container / list-like data structure to contain the elements queued up by the player during gameplay.

---
+++ UnityGame/GBS/Assets/Scripts/Classes/ElementQueue.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementQueue
+{
+    #region Variables
+    #endregion
+
+    #region Constructor
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
