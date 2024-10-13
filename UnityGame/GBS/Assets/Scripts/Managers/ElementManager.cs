c26cf1c094f0cef6a888596688e61e7581b89725
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-13 17:47:46 +0200

    Created input serializable structs for element manager
    These structs will be used to manage the element data in the init scene by manually editing the elemental configuration in the element manager.

--- UnityGame/GBS/Assets/Scripts/Managers/ElementManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/ElementManager.cs
@@ -5,6 +5,27 @@ using UnityEngine;
 public class ElementManager : SingletonPersistent<ElementManager>
 {
     #region Structs
+
+    // This struct will be used to determine the elemental combinations
+    // A + B = C
+    // Opposite element cancellations are basically combination operations where C is Element.None
+    [System.Serializable]
+    public struct InputElementTuple
+    {
+        public Element elementA;
+        public Element elementB;
+        public Element elementC;
+    }
+
+    // This struct will be used to assign an image to every single element when used in game during element queuing.
+    // NOTE : The buttons in the player UI have hardcoded images, maybe I should use a slightly different image for those?
+    [System.Serializable]
+    public struct InputElementImage
+    {
+        public Element element;
+        public Sprite image;
+    }
+
     #endregion
 
     #region Variables
