a2c59048e075581ba6d0699ceda0e3c5c25c3ff4
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-14 04:02:55 +0200

    Created spell caster controller
    A controller script whose purpose is to control the casting of spells.
    
    Basically, this is a sort of WeaponsController or WeaponsSystemController that allows an entity to cast spells.
    
    Used for the player and other spell-caster type entities.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/SpellCasterController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCasterController : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private Transform spawnTransform;
+
+    #endregion
+
+    #region MonoBehaviour
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
