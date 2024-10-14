4776917d3a21c2041cc0e67020212fdf3636375a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-14 21:34:00 +0200

    Added code for debug manager line drawing
    Yes, the code is weird and it's a hack, but it's done like this because I want the debug draws to work during runtime on the build... users deserve to know!

--- UnityGame/GBS/Assets/Scripts/Managers/DebugManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/DebugManager.cs
@@ -6,19 +6,22 @@ using UnityEngine;
 public class DebugManager : Singleton<DebugManager>
 {
     #region Variables
+    
     [SerializeField] private bool debugEnabled = false;
+    [SerializeField] private LineRenderer lineRenderer;
+
     #endregion
 
     #region MonoBehaviour
 
     void Start()
     {
-        
+        this.UpdateDebugComponents(); // Update all debug components on start
     }
 
     void Update()
     {
-        
+
     }
 
     #endregion
@@ -40,6 +43,7 @@ public class DebugManager : Singleton<DebugManager>
     public void SetDebugEnabled(bool value)
     {
         this.debugEnabled = value;
+        this.UpdateDebugComponents();
     }
 
     public bool GetDebugEnabled()
@@ -47,8 +51,25 @@ public class DebugManager : Singleton<DebugManager>
         return this.debugEnabled;
     }
 
+    public void DrawLine(Color color, Vector3 start, Vector3 end)
+    {
+        if (this.debugEnabled && this.lineRenderer != null)
+        {
+            Vector3[] positions = { start, end };
+            this.lineRenderer.SetPositions(positions);
+            this.lineRenderer.startColor = color;
+            this.lineRenderer.endColor = color;
+        }
+    }
+
     #endregion
 
     #region PrivateMethods
+
+    private void UpdateDebugComponents()
+    {
+        this.lineRenderer.gameObject.SetActive(this.debugEnabled);
+    }
+
     #endregion
 }
