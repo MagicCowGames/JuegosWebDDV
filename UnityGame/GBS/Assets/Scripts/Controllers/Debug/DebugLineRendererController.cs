0ff4beca721967e9e2fe75642cc87b7a7b018bb8
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-14 23:21:00 +0200

    Fixed issue where LineRenderer was used before it was set
    The debug line renderer controller made use of Unity's GetComponent<> function to get the LineRenderer component at the start.
    
    This is a problem because the order in which start is executed is not guaranteed to be as soon as the object is created, which means that the debug manager could be referencing null data.
    
    This could be partially fixed using awake, but the best solution (and the one implemented) is to set the line renderer as a parameter on the inspector within the debug line renderer prefab.

--- UnityGame/GBS/Assets/Scripts/Controllers/Debug/DebugLineRendererController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Debug/DebugLineRendererController.cs
@@ -15,7 +15,10 @@ public class DebugLineRendererController : MonoBehaviour
 
     void Start()
     {
-        this.lineRenderer = GetComponent<LineRenderer>();
+        // This component is now gotten from the inspector panel so we don't need to get it from here anymore.
+        // Getting it from here used to cause some issues with race conditions where sometimes, the component was assigned to the lineRenderer variable
+        // AFTER it had been used in the code that calls GetLineRenderer() on the DebugManager, meaning that we would get a dumb null pointer exception...
+        // this.lineRenderer = GetComponent<LineRenderer>();
         this.usedLastFrame = false;
     }
 
