2426f0fed4aab47f7ab7aad1dffb00c42502d973
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-14 22:02:49 +0200

    Implemented dynamic pool with dynamic reallocation

--- UnityGame/GBS/Assets/Scripts/Utility/ObjectPool.cs
+++ UnityGame/GBS/Assets/Scripts/Utility/ObjectPool.cs
@@ -2,25 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Object Pool implementation with dynamic reallocation / capcity growth
 public class ObjectPool
 {
     #region Variables
 
-    public GameObject Prefab;
-    public GameObject[] Objects;
-    public int Capacity;
-    public int Count;
+    public GameObject Prefab { get; private set; }
+    public List<GameObject> Objects { get; private set; }
+    public int Capacity { get; private set; }
+    public int MaxCapacity { get; private set; }
+    public int Count { get; private set; }
 
     #endregion
 
     #region Constructor
 
-    public ObjectPool(GameObject prefab, int capacity)
+    public ObjectPool(GameObject prefab, int initialCapacity, int maxCapacity)
     {
         this.Count = 0;
         this.Prefab = prefab;
-        this.Capacity = capacity;
-        this.Objects = new GameObject[this.Capacity];
+        this.Capacity = initialCapacity;
+        this.MaxCapacity = maxCapacity;
+        this.Objects = new List<GameObject>(this.Capacity);
         for (int i = 0; i < this.Capacity; ++i)
         {
             this.Objects[i] = ObjectSpawner.Spawn(this.Prefab);
@@ -39,6 +42,14 @@ public class ObjectPool
         {
             GameObject obj = this.Objects[this.Count];
             obj.SetActive(true);
+            ++this.Count;
+        }
+        else
+        if (this.Count < this.MaxCapacity)
+        {
+            var obj = ObjectSpawner.Spawn(this.Prefab);
+            obj.SetActive(true);
+            this.Objects.Add(obj);
             ++this.Count;
         }
         return ans;
