247220f6e6161e9e76db869466193bc6301ab8ce
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-14 23:22:48 +0200

    Created new debug draw methods in the debug manager
    The debug manager now has 2 new render methods:
    
    DrawSegment() : draws a segment that connects multiple points.
    DrawSphere() : uses draw segment to draw a simple debug sphere.

--- UnityGame/GBS/Assets/Scripts/Managers/DebugManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/DebugManager.cs
@@ -70,6 +70,9 @@ public class DebugManager : Singleton<DebugManager>
 
     #region PublicMethods - Drawing
 
+    // NOTE : The only reason this fucking mess exists is literally to allow debug drawing on the final release build to be enabled through the console.
+    // That's all there is to it.
+
     public void DrawLine(Color color, Vector3 start, Vector3 end)
     {
         if (!this.debugEnabled)
@@ -78,11 +81,84 @@ public class DebugManager : Singleton<DebugManager>
         var lineRenderer = this.GetLineRenderer();
 
         Vector3[] positions = { start, end };
+        lineRenderer.lineRenderer.positionCount = 2;
         lineRenderer.lineRenderer.SetPositions(positions);
         lineRenderer.lineRenderer.startColor = color;
         lineRenderer.lineRenderer.endColor = color;
+        lineRenderer.lineRenderer.loop = false;
 
-        Debug.Log("WTF");
+        lineRenderer.lineRenderer.startWidth = 0.1f;
+        lineRenderer.lineRenderer.endWidth = 0.1f;
+    }
+
+    public void DrawSegment(Color color, Vector3[] points, bool loop = false)
+    {
+        if (!this.debugEnabled)
+            return;
+
+        var lineRenderer = this.GetLineRenderer();
+
+        lineRenderer.lineRenderer.positionCount = points.Length;
+        lineRenderer.lineRenderer.SetPositions(points);
+        lineRenderer.lineRenderer.startColor = color;
+        lineRenderer.lineRenderer.endColor = color;
+        lineRenderer.lineRenderer.loop = loop;
+
+        lineRenderer.lineRenderer.startWidth = 0.1f;
+        lineRenderer.lineRenderer.endWidth = 0.1f;
+    }
+
+    public void DrawSphere(Color color, Vector3 origin, float radius = 5.0f)
+    {
+        if (!this.debugEnabled)
+            return;
+
+        Vector3[] points1 = {
+            new Vector3(0, -1, 0),
+            new Vector3(0.707107f, -0.707107f, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(0.707107f, 0.707107f, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(-0.707107f, 0.707107f, 0),
+            new Vector3(-1, 0, 0),
+            new Vector3(-0.707107f, -0.707107f, 0)
+        };
+
+        Vector3[] points2 = {
+            new Vector3(0, -1, 0),
+            new Vector3(0, -0.707107f, 0.707107f),
+            new Vector3(0, 0, 1),
+            new Vector3(0, 0.707107f, 0.707107f),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 0.707107f, -0.707107f),
+            new Vector3(0, 0, -1),
+            new Vector3(0, -0.707107f, -0.707107f)
+        };
+
+        Vector3[] points3 = {
+            new Vector3(1, 0, 0),
+            new Vector3(0.707107f, 0, 0.707107f),
+            new Vector3(0, 0, 1),
+            new Vector3(-0.707107f, 0, 0.707107f),
+            new Vector3(-1, 0, 0),
+            new Vector3(-0.707107f, 0, -0.707107f),
+            new Vector3(0, 0, -1),
+            new Vector3(0.707107f, 0, -0.707107f)
+        };
+
+        for (int i = 0; i < points1.Length; ++i)
+            points1[i] = origin + points1[i] * radius;
+
+
+        for (int i = 0; i < points2.Length; ++i)
+            points2[i] = origin + points2[i] * radius;
+
+        for (int i = 0; i < points3.Length; ++i)
+            points3[i] = origin + points3[i] * radius;
+
+        DrawSegment(color, points1, true);
+        DrawSegment(color, points2, true);
+        DrawSegment(color, points3, true);
     }
 
     #endregion
