ce38c91705ae16aa8fdb4eab63b2ed19667ef8a3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-14 04:14:51 +0200

    Added base for Spell casting implementation
    The base of the implementation consists of a set of if conditions that check the composition of the spell, determining what type of category the spell will be.

--- UnityGame/GBS/Assets/Scripts/Controllers/SpellCasterController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/SpellCasterController.cs
@@ -7,6 +7,9 @@ public class SpellCasterController : MonoBehaviour
     #region Variables
 
     [SerializeField] private Transform spawnTransform;
+    
+    // TODO : Implement this system in a somewhat clean way...?
+    // [SerializeField] private GameObject parentTarget; // The target entity that owns this spell caster. This is the entity to which the self-cast spells must be applied to.
 
     #endregion
 
@@ -25,6 +28,47 @@ public class SpellCasterController : MonoBehaviour
     #endregion
 
     #region PublicMethods
+
+    public void Cast(ElementQueue queue)
+    {
+        // If the queue is null or the queue has no elements queued up, then we return because there is nothing else to be done.
+        if (queue == null || queue.Count == 0)
+            return;
+        
+        if (queue.GetElementCount(Element.Shield) > 0)
+        {
+            DebugManager.Instance?.Log("Shield-like spell");
+            return;
+        }
+
+        if (queue.GetElementCount(Element.Earth) > 0)
+        {
+            DebugManager.Instance?.Log("Projectile-like spell");
+            return;
+        }
+
+        if (queue.GetElementCount(Element.Ice) > 0)
+        {
+            DebugManager.Instance?.Log("Shard-like spell");
+            return;
+        }
+
+        if (queue.GetElementCount(Element.Heal) > 0 || queue.GetElementCount(Element.Death) > 0)
+        {
+            DebugManager.Instance?.Log("Beam-like spell");
+            return;
+        }
+
+        if (queue.GetElementCount(Element.Electricity) > 0)
+        {
+            DebugManager.Instance?.Log("Electric-like spell");
+            return;
+        }
+
+        DebugManager.Instance?.Log("Spray-like spell");
+        return;
+    }
+
     #endregion
 
     #region PrivateMethods
