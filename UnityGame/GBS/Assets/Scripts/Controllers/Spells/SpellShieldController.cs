32f86d57a44d0d5070f4e736ceb74760920151a4
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-15 23:25:43 +0200

    Shield controller animation and lifetime logic implemented
    Shields / walls now have a simple state machine that controls the logic for spawning walls, moving them up from the ground, waiting for a time, burying them underground and then despawning the walls.

--- UnityGame/GBS/Assets/Scripts/Controllers/Spells/SpellShieldController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Spells/SpellShieldController.cs
@@ -4,22 +4,69 @@ using UnityEngine;
 
 public class SpellShieldController : MonoBehaviour
 {
+    #region Enums
+
+    private enum ShieldAnimStatus
+    {
+        MovingUp = 0,
+        Idle,
+        MovingDown,
+        Finish
+    }
+
+    #endregion
+
     #region Variables
+
+    [SerializeField] private float lifeTime = 5.0f;
+
+    [SerializeField] private Transform shieldTransform;
+    [SerializeField] private Transform startTransform;
+    [SerializeField] private Transform idleTransform;
+
+    [SerializeField] private float animSpeed = 10.0f;
+
+    private ShieldAnimStatus animStatus;
 
     #endregion
 
     #region MonoBehaviour
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        this.animStatus = ShieldAnimStatus.MovingUp;
+        this.shieldTransform.position = this.startTransform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float delta = Time.deltaTime;
+        float speed = this.animSpeed;
+        Transform target;
+
+        switch (this.animStatus)
+        {
+            case ShieldAnimStatus.MovingUp:
+                target = this.idleTransform;
+                if (MoveToTarget(target, speed, delta))
+                    this.animStatus = ShieldAnimStatus.Idle;
+                break;
+            case ShieldAnimStatus.Idle:
+                this.lifeTime -= delta;
+                if(this.lifeTime < 0.0f)
+                    this.animStatus = ShieldAnimStatus.MovingDown;
+                break;
+            case ShieldAnimStatus.MovingDown:
+                target = this.startTransform;
+                if (MoveToTarget(target, speed, delta))
+                    this.animStatus = ShieldAnimStatus.Finish;
+                break;
+            default:
+            case ShieldAnimStatus.Finish:
+                this.gameObject.SetActive(false);
+                Destroy(this.gameObject);
+                break;
+        }
     }
 
     #endregion
@@ -28,5 +75,16 @@ public class SpellShieldController : MonoBehaviour
     #endregion
 
     #region PrivateMethods
+
+    private bool MoveToTarget(Transform target, float speed, float delta)
+    {
+        if (Vector3.Distance(this.shieldTransform.position, target.position) > 0.01f)
+        {
+            this.shieldTransform.position = Vector3.Lerp(this.shieldTransform.position, target.position, Mathf.Clamp01(delta * speed));
+            return false;
+        }
+        return true;
+    }
+
     #endregion
 }
