22746252764f1a5d53f69ab9c9c1a435e70b58b0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-15 03:56:25 +0200

    Added extra functionality to health controller
    The health controller now has methods to get information such as the current health percentage, whether the owner of the health is alive or not, to force healing to a given value out of the min-max bounds and to fully restore the health to its max value.

--- UnityGame/GBS/Assets/Scripts/Controllers/HealthController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/HealthController.cs
@@ -32,6 +32,33 @@ public class HealthController : MonoBehaviour
     #endregion
 
     #region PublicMethods
+
+    public float GetPercentage()
+    {
+        // NOTE : We don't care about dividing by 0 because these are not integer values.
+        // Division by 0 is a perfectly valid operation for floating point values.
+        // Now, whether it gives a value that is useable or even consistent or not is a whole other story,
+        // but what matters is that the universe does not explode if we don't make this check.
+        // Besides, who the fuck would set the min and max to the same value? Am I right? (famous last words)
+        return this.health / (this.healthMax - this.healthMin);
+    }
+
+    public bool IsAlive()
+    {
+        // har har har such a smart function!!! Yes, I know, this is indeed a real thing, leave me alone...
+        return this.health > 0.0f;
+    }
+
+    public void ForceSetHealth(float value)
+    {
+        this.health = value;
+    }
+
+    public void Heal()
+    {
+        this.health = this.HealthMax;
+    }
+
     #endregion
 
     #region PrivateMethods
