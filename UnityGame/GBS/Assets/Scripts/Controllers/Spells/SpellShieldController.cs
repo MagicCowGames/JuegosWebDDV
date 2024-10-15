9381d39920e4310289c3dae4ef0ea0ef8b634898
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-15 19:20:42 +0200

    Created Spell Shield Controller
    WIP : Shield spells are not implemented yet.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/Spells/SpellShieldController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellShieldController : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region MonoBehaviour
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
