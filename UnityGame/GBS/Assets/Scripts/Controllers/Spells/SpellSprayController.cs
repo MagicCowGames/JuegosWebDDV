c54895674067855d6e7dccc061b070f6ed887e3a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-16 20:01:51 +0200

    Created spell spray controller
    Sprays and elemental walls make use of this controller.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/Spells/SpellSprayController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellSprayController : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private ParticleSystem sprayParticleSystem;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
