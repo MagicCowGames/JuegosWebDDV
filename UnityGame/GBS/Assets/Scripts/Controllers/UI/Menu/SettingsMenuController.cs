7328f4bd107fd03f99ea23f18f5110b0a04311bd
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-25 14:37:18 +0200

    Wired components from settings menu scene to controller

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/SettingsMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/SettingsMenuController.cs
@@ -4,11 +4,13 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+// TODO : Make the settings SCREEN its own thing so that we can reuse that prefab both in game on the pause menu's UI and on the main menu's settings menu UI.
 public class SettingsMenuController : UIController
 {
     #region Variables
 
-    [Header("Buttons")]
+    [Header("Settings - Language")]
+    [SerializeField] private TMP_Text languageText; // The currently selected language.
     [SerializeField] private Button buttonLanguageLeft;
     [SerializeField] private Button buttonLanguageRight;
 
