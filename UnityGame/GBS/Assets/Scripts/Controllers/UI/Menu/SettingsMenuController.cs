16b4b49297675c62b7d6559260daee606411a265
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-25 14:34:53 +0200

    Created basic Settings Menu Controller script
    This script will be in charge of controlling the settings menu's logic.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/SettingsMenuController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenuController : UIController
+{
+    #region Variables
+
+    [Header("Buttons")]
+    [SerializeField] private Button buttonLanguageLeft;
+    [SerializeField] private Button buttonLanguageRight;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
