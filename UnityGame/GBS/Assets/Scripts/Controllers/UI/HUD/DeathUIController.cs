b76455cb5cf682a944a521d3640635b22233091f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-27 20:12:21 +0100

    Created DeathUIController script
    Script that controls the logic of the death UI screen.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/DeathUIController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathUIController : UIController
+{
+    #region Variables
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    // NOTE : As of now, it simply reloads the only scene we have.
+    // TODO : Change implementation to get current level name and reload from start rather than hardcoding the level to be loaded.
+    // Maybe even add checkpoint support if you find it to be cool with this game's gameplay.
+    public void Button_Retry()
+    {
+        SceneLoadingManager.Instance?.LoadScene("DebugSceneGameplay");
+    }
+
+    public void Button_ReturnToMenu()
+    {
+        GameUtility.Resume(); // Resume before loading main menu
+        SceneLoadingManager.Instance?.LoadSceneMainMenu();
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
