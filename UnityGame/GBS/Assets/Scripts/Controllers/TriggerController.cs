7e398cb5ac9357b0e3c4e5f5052058e80d9d53af
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-27 22:44:11 +0100

    Implemented basic logic for trigger controller

--- UnityGame/GBS/Assets/Scripts/Controllers/TriggerController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/TriggerController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+// NOTE : For now, only the player can trigger these. We will make this configurable in the future by adding entity controller scripts which will be gotten on trigger
+// and used to determine what type of entity has collided with the trigger.
 public class TriggerController : MonoBehaviour
 {
     #region Variables
+
+    [SerializeField] private UnityEvent OnTrigger;
 
     #endregion
 
@@ -26,5 +31,18 @@ public class TriggerController : MonoBehaviour
     #endregion
 
     #region PrivateMethods
+    #endregion
+
+    #region Collisions
+
+    void OnTriggerEnter(Collider other)
+    {
+        var obj = other.gameObject;
+        var player = obj.GetComponent<PlayerController>();
+        if (player == null)
+            return;
+        OnTrigger?.Invoke();
+    }
+
     #endregion
 }
