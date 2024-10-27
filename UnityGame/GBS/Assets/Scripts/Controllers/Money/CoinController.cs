e5fe2b8aecbb1bd29c7e2f44c00bfac1a8e62a42
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-10-27 22:05:45 +0100

    Implemented coin pickup logic
    A very basic implementation that is pending revision in the future. Allows entities with a MoneyController ("pouch") to pick up coins.

--- UnityGame/GBS/Assets/Scripts/Controllers/Money/CoinController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Money/CoinController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// NOTE : This should have been named MoneyPickupController or some shit tbh.
 [ExecuteInEditMode]
 public class CoinController : MonoBehaviour
 {
@@ -70,13 +71,19 @@ public class CoinController : MonoBehaviour
                 break;
         }
 
-        // TODO : Change visual mesh and material for the selected coint type, etc...
-        /*
+        // TODO : Change visual mesh and material for the selected coint type, etc... for now it only changes the visual material and calls it a day. Need to pick
+        // random money pile mesh, etc...
+        // TODO : Change this shit to a simple set translating the fucking coin type to an integer and picking the value from an array
         switch (this.coinType)
         {
-
+            default:
+            case CoinType.Copper:
+                break;
+            case CoinType.Silver:
+                break;
+            case CoinType.Gold:
+                break;
         }
-        */
     }
 
     #endregion
@@ -85,5 +92,23 @@ public class CoinController : MonoBehaviour
     #endregion
 
     #region PrivateMethods
+    #endregion
+
+    #region Collisions
+
+    // PickUp logic goes here... should probably encapsulate this and make this controller inherit from some base PickUp controller of sorts.
+    // It would either check for whatever specific "container" component it requires for its item type (could be a PickUp<T> thing maybe)
+    // or it would just check for a "Picker" controller, or whatever, or maybe have a Picker/ItemContainer base controller and the Money and Potions ones
+    // derive from it and we just call the base one and add the value and let each one write its own impl... etc etc... for now, this will suffice.
+    void OnTriggerEnter(Collider other)
+    {
+        var obj = other.gameObject;
+        var money = other.GetComponent<MoneyController>();
+        if (money == null)
+            return;
+        money.Money += this.valueTotal;
+        Destroy(this.gameObject); // Maybe do some pooling or some shit in the future?
+    }
+
     #endregion
 }
