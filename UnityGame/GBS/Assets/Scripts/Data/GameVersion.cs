232cb9d040fbef2a0658660dd4eeb1737868ed71
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-01 04:41:53 +0100

    Added version comparison functionality
    The GameVersion struct now has a set of methods that can be used to determine which version is higher or lower.
    
    This may be useful in the future to translate old save data to a more modern format function by function.

--- UnityGame/GBS/Assets/Scripts/Data/GameVersion.cs
+++ UnityGame/GBS/Assets/Scripts/Data/GameVersion.cs
@@ -1,20 +1,68 @@
 public enum GameVersionType
 {
     Alpha = 0,
-    Beta,
-    Release
+    Beta = 1,
+    Release = 2
 }
 
 // Struct used to identify a version of the game.
 // NOTE : This struct can also be used to perform save data translation functions in case that the save format structure changes between
 // versions by adding incremental save methods.
-// TODO : Add a set of comparator methods to determine which version is lower or higher.
 [System.Serializable]
 public struct GameVersion
 {
+    #region Variables
+
     public int major;
     public int minor;
     public int patch;
     public GameVersionType type;
     public string name; // Specific name given to the version, reserved for special versions.
+
+    #endregion
+
+    #region PublicMethods
+
+    public static bool IsEqual(GameVersion a, GameVersion b)
+    {
+        return
+            a.type == b.type &&
+            a.major == b.major &&
+            a.minor == b.minor &&
+            a.patch == b.patch
+            ;
+    }
+
+    public static bool IsLower(GameVersion a, GameVersion b)
+    {
+        if ((int)a.type > (int)b.type)
+            return false;
+
+        if (a.major > b.major)
+            return false;
+        if (a.minor > b.minor)
+            return false;
+        if (a.patch > b.patch)
+            return false;
+        
+        return true;
+    }
+
+    public static bool IsHigher(GameVersion a, GameVersion b)
+    {
+        return IsLower(b, a);
+    }
+
+    public static int Compare(GameVersion a, GameVersion b)
+    {
+        if (IsEqual(a, b))
+            return 0;
+        if (IsLower(a, b))
+            return -1;
+        if (IsHigher(a, b))
+            return 1;
+        return 0; // Assume that any edge cases should also point to equal versions.
+    }
+
+    #endregion
 }
