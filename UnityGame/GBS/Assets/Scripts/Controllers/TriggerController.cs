f383af09b41acb9dcc0e889b45e923f26863f2e7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-01 14:51:48 +0100

    Improved TriggerController
    The trigger controller has been improved to add new functionality.
    
    Triggers can now trigger other triggers through on trigger calls.
    Triggers now differentiate manually activated triggering calls from trigger area events.
    
    Trigger areas can now launch events on trigger area enter, on exit and on stay.

--- UnityGame/GBS/Assets/Scripts/Controllers/TriggerController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/TriggerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,7 +10,13 @@ public class TriggerController : MonoBehaviour
 {
     #region Variables
 
-    [SerializeField] private UnityEvent OnTrigger;
+    // [SerializeField] private string triggerName;
+    [SerializeField] private UnityEvent OnTrigger; // This one runs when the trigger is activated through some script or another trigger. Will mostly go unused.
+    [SerializeField] private UnityEvent OnTriggerAreaEnter; // This one runs when an entity enters the trigger's area
+    [SerializeField] private UnityEvent OnTriggerAreaExit; // This one runs when an entity exits the trigger's area
+    [SerializeField] private UnityEvent OnTriggerAreaStay; // This one runs when an entity stays on the trigger's area
+
+    // TODO : Add bools or bitmask to determine what type of entities can activate this trigger.
 
     #endregion
 
@@ -28,20 +35,53 @@ public class TriggerController : MonoBehaviour
     #endregion
 
     #region PublicMethods
+
+    public void Trigger()
+    {
+        this.OnTrigger?.Invoke();
+    }
+
     #endregion
 
     #region PrivateMethods
+
+    // Determines if the input gameobject can activate this trigger
+    // The trigger can only be activated by gameobjects that are of a certain set of "entity" types specific to the game, such as players, NPCs and whatnot.
+    private bool CanTrigger(GameObject obj)
+    {
+        var player = obj.GetComponent<PlayerController>();
+        // TODO : Add support for other types of entities appart from the player by adding checks for other controller components
+        bool ans =
+            player != null // || whatever != null || etc...
+            ;
+        return ans;
+    }
+
+    private bool CanTrigger(Collider collider)
+    {
+        return CanTrigger(collider.gameObject);
+    }
+
     #endregion
 
     #region Collisions
 
     void OnTriggerEnter(Collider other)
     {
-        var obj = other.gameObject;
-        var player = obj.GetComponent<PlayerController>();
-        if (player == null)
-            return;
-        OnTrigger?.Invoke();
+        if(CanTrigger(other))
+            OnTriggerAreaEnter?.Invoke();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(CanTrigger(other))
+            OnTriggerAreaExit?.Invoke();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(CanTrigger(other))
+            OnTriggerAreaStay?.Invoke();
     }
 
     #endregion
