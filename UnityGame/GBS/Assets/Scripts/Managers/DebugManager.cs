7ee8497bd06f916878a30e1a42cb09771a879490
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-05 17:40:01 +0100

    Added box drawing functions to debug manager
    The debug manager can now draw box shapes.

--- UnityGame/GBS/Assets/Scripts/Managers/DebugManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/DebugManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DebugManager : Singleton<DebugManager>
 {
@@ -80,6 +81,9 @@ public class DebugManager : Singleton<DebugManager>
 
     // NOTE : The only reason this fucking mess exists is literally to allow debug drawing on the final release build to be enabled through the console.
     // That's all there is to it.
+    
+    // Looking back at this code, it isn't really that much of a mess, but it really is annoying having had to implement this system
+    // by hand when Unity already offers its own debug drawing methods... but it doesn't allow packing them in release builds!!!
 
     public void DrawLine(Vector3 start, Vector3 end, Color color, float width = 0.1f)
     {
@@ -105,6 +109,64 @@ public class DebugManager : Singleton<DebugManager>
 
         lineRenderer.lineRenderer.startWidth = width;
         lineRenderer.lineRenderer.endWidth = width;
+    }
+
+    public void DrawBox(Vector3 origin, float sideLength, Color color, float width = 0.1f)
+    {
+        if (!this.debugEnabled)
+            return;
+        DrawBox(origin, sideLength, sideLength, sideLength, color, width);
+    }
+
+    public void DrawBox(Vector3 origin, float lengthX, float lengthY, float lengthZ, Color color, float width = 0.1f)
+    {
+        if (!this.debugEnabled)
+            return;
+        DrawBox(origin, new Vector3(lengthX, lengthY, lengthZ), color, width);
+    }
+
+    public void DrawBox(Vector3 origin, Vector3 sideLengths, Color color, float width = 0.1f)
+    {
+        if (!this.debugEnabled)
+            return;
+
+        Vector3[] points1 = {
+            new Vector3(-1, -1, -1),
+            new Vector3(1, -1, -1),
+            new Vector3(1, -1, 1),
+            new Vector3(-1, -1, 1)
+        };
+        
+        Vector3[] points2 = {
+            new Vector3(-1, 1, -1),
+            new Vector3(-1, -1, -1),
+            new Vector3(-1, -1, 1),
+            new Vector3(-1, 1, 1)
+        };
+
+        Vector3[] points3 = {
+            new Vector3(1, 1, -1),
+            new Vector3(-1, 1, -1),
+            new Vector3(-1, 1, 1),
+            new Vector3(1, 1, 1)
+        };
+
+        Vector3[] points4 = {
+            new Vector3(1, -1, -1),
+            new Vector3(1, 1, -1),
+            new Vector3(1, 1, 1),
+            new Vector3(1, -1, 1)
+        };
+
+        ApplyScaleSegmentBox(origin, points1, sideLengths);
+        ApplyScaleSegmentBox(origin, points2, sideLengths);
+        ApplyScaleSegmentBox(origin, points3, sideLengths);
+        ApplyScaleSegmentBox(origin, points4, sideLengths);
+
+        DrawSegment(points1, false, color, width);
+        DrawSegment(points2, false, color, width);
+        DrawSegment(points3, false, color, width);
+        DrawSegment(points4, false, color, width);
     }
 
     public void DrawSphere(Vector3 origin, float radius, Color color, float width = 0.1f)
@@ -196,6 +258,24 @@ public class DebugManager : Singleton<DebugManager>
         ans.gameObject.SetActive(true);
 
         return ans;
+    }
+
+    #endregion
+
+    #region PrivateMethods - Drawing Internals
+
+    // This region contains private methods used internally by the public drawing methods.
+    // Their only purpose is to facilitate some od the drawing code by reducing code repetition and such.
+
+    private Vector3[] ApplyScaleSegmentBox(Vector3 origin, Vector3[] points, Vector3 sideLengths)
+    {
+        for (int i = 0; i < points.Length; ++i)
+        {
+            points[i].x = origin.x + points[i].x * (sideLengths.x * 0.5f);
+            points[i].y = origin.y + points[i].y * (sideLengths.y * 0.5f);
+            points[i].z = origin.z + points[i].z * (sideLengths.z * 0.5f);
+        }
+        return points;
     }
 
     #endregion
