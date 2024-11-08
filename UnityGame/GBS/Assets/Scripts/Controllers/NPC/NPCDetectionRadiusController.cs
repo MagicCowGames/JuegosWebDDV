d223339329dde85b15704b8bfaed09c87d7617f7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-08 16:57:13 +0100

    Implemented basic player detection in DetectionRadius controller
    The NPCDetectionRadius controller now implements the logic required to detect the player on overlap with the detection area.
    
    The ai controller is notified and its target is updated to the game object of the palyer character.

--- UnityGame/GBS/Assets/Scripts/Controllers/NPC/NPCDetectionRadiusController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/NPCDetectionRadiusController.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO : Possibly replace this with a regular trigger when the Entity system is implemented?
+// Or keep it like this since the detection radius controller could be refactored into a more generic detection areas controller with a detection radius
+// and a capsule for sight, and maybe some hearing abilities, etc...
 public class NPCDetectionRadiusController : MonoBehaviour
 {
     #region Variables
+
+    [SerializeField] private TestDummyController aiController;
+
     #endregion
 
     #region MonoBehaviour
@@ -25,5 +31,28 @@ public class NPCDetectionRadiusController : MonoBehaviour
     #endregion
 
     #region PrivateMethods
+
+    private void DetectEntity(GameObject obj)
+    {
+        // TODO : Modify system to detect any kind of entity with a team system in the future. For now, just detect the player since all NPCs will be enemies for now.
+        // var entity = obj.GetComponent<Entity>();
+
+        var player = obj.GetComponent<PlayerController>();
+
+        if (player == null) // If the GO is not a player, then we bail and do nothing.
+            return;
+
+        this.aiController.Target = obj;
+    }
+
+    #endregion
+
+    #region Collisions
+
+    void OnTriggerEnter(Collider other)
+    {
+        DetectEntity(other.gameObject);
+    }
+
     #endregion
 }
