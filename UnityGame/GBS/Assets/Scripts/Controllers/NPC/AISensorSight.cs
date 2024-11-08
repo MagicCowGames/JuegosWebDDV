cf402cf2d2878e50e5421bcdb7cb82f9d5f5cd8b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-08 21:12:57 +0100

    Fixed issue with direction vector in AISensorSight
    Fixed an oversight where the direction vector in AISensorSight was always the forward vector. The vector to the target is now properly calculated.

--- UnityGame/GBS/Assets/Scripts/Controllers/NPC/AISensorSight.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/AISensorSight.cs
@@ -13,8 +13,13 @@ public class AISensorSight : AISensorBase
 
         // Try to see if there's a direct line of sight to the player. If the hit is true and the hit target is not the player, then we quit.
         RaycastHit hit;
-        bool hasHit = Physics.Raycast(this.transform.position, this.transform.forward, out hit);
-        if (hasHit && hit.collider.gameObject.GetComponent<PlayerController>() != null)
-            this.OnSense(obj);
+        Vector3 origin = this.transform.position;
+        Vector3 direction = obj.transform.position - this.transform.position;
+        bool hasHit = Physics.Raycast(origin, direction, out hit);
+        if (hasHit && obj != hit.collider.gameObject) // This check is kinda crap actually since it's always going to hit something... or it should at least!
+            return;
+        
+        this.OnSense(obj);
+        DebugManager.Instance?.DrawLine(this.transform.position, hit.point, Color.red); // Debug sight line trace.
     }
 }
