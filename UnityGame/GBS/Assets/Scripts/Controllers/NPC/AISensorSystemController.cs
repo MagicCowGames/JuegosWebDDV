09703c787dd60eaac36d2b5acf7461bfeac09f67
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-08 20:48:15 +0100

    Modified AISensorSystemController to assign sensor event callbacks

--- UnityGame/GBS/Assets/Scripts/Controllers/NPC/AISensorSystemController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/AISensorSystemController.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // TODO : Possibly replace this with a regular trigger when the Entity system is implemented?
-// Or keep it like this since the detection radius controller could be refactored into a more generic detection areas controller with a detection radius
-// and a capsule for sight, and maybe some hearing abilities, etc...
+// TODO : Finish implementing logic for all the other sensors, such as hearing and stuff like that.
 public class AISensorController : MonoBehaviour
 {
     #region Variables
 
     [Header("AI Controller")]
-    [SerializeField] private TestDummyController aiController;
+    [SerializeField] private TestDummyController aiController; // TODO : Move this logic so that it is the AI controller itself that handles this detect method stuff. This class should be just like the sensor classes and notify the AI controller through a callback or event.
 
     [Header("Sensors")]
-    [SerializeField] private SphereCollider radiusCollider;
-    [SerializeField] private CapsuleCollider sightCollider;
+    [SerializeField] private AISensorBase[] sensors;
 
     #endregion
 
@@ -22,7 +20,9 @@ public class AISensorController : MonoBehaviour
 
     void Start()
     {
-        
+        // Register the events for the sensors' callbacks.
+        foreach (var sensor in sensors)
+            sensor.OnSense += DetectEntity;
     }
 
     void Update()
@@ -37,26 +37,12 @@ public class AISensorController : MonoBehaviour
 
     #region PrivateMethods
 
+    // TODO : Modify the code within the sensors system to detect any kind of entity with a team system in the future. This could be done by having a bool checking
+    // function within the base sensor class.
+    // For now, just detect the player since all NPCs will be enemies for now.
     private void DetectEntity(GameObject obj)
     {
-        // TODO : Modify system to detect any kind of entity with a team system in the future. For now, just detect the player since all NPCs will be enemies for now.
-        // var entity = obj.GetComponent<Entity>();
-
-        var player = obj.GetComponent<PlayerController>();
-
-        if (player == null) // If the GO is not a player, then we bail and do nothing.
-            return;
-
-        this.aiController.Target = obj;
-    }
-
-    #endregion
-
-    #region Collisions
-
-    void OnTriggerEnter(Collider other)
-    {
-        DetectEntity(other.gameObject);
+        this.aiController.Target = obj; // The player is instantly detected and targetted.
     }
 
     #endregion
