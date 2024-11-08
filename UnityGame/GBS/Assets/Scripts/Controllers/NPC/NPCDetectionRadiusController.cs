3b951184ce12203d1476f37a00020b24fcf2e74c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-08 16:47:54 +0100

    Created detection radius controller fro NPCs
    This component is in charge of detecting entities that enter the sensor / detection radius of NPCs.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/NPCDetectionRadiusController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDetectionRadiusController : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
