b746a01b6a50631e28b15c023cb0228fee2f2cd1
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-08 20:47:35 +0100

    Created AISensorHear controller script
    TODO : Implement sound hearing system.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/AISensorHear.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO : Implement (will come after I build the whole sound object pool shenanigans that I have in mind)
+public class AISensorHear : MonoBehaviour
+{
+}
