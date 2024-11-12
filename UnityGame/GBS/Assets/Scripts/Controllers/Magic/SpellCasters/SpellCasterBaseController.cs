17176fc65d4dae0f8e71b20ef6d5503e8f8a43c3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-12 06:06:35 +0100

    Implemented ISpellCaster interface for SpellCasterBaseController
    The SpellCasterBaseController has now been implemented. This base class will be inherited from by other specific casters to implement the specific spell caster behaviour for different spell types.

--- UnityGame/GBS/Assets/Scripts/Controllers/Magic/SpellCasters/SpellCasterBaseController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Magic/SpellCasters/SpellCasterBaseController.cs
@@ -9,11 +9,14 @@ public class SpellCasterBaseController : MonoBehaviour, ISpellCaster
     // NOTE : Each spell caster is responsible for dealing with its own spawn transforms and prefabs.
 
     [Header("Spell Caster Base Controller")]
-    [SerializeField] protected GameObject[] spellPrefabs;
+    [SerializeField] protected GameObject[] spellPrefabs; // NOTE : Maybe the magic manager should be the one to have a field with all of the prefabs stored so that we do not have multiple copies needlessly stored in memory?
     [SerializeField] protected Transform[] spawnTransforms;
 
+    // protected Form form;
     protected ElementQueue elementQueue;
-    private bool isCasting;
+    protected bool isCasting;
+
+    protected float castDuration;
 
     #endregion
 
@@ -46,6 +49,7 @@ public class SpellCasterBaseController : MonoBehaviour, ISpellCaster
         // Initialize variables to default values.
         this.elementQueue = null;
         this.isCasting = false;
+        this.castDuration = 0.0f;
     }
 
     #endregion
@@ -57,33 +61,84 @@ public class SpellCasterBaseController : MonoBehaviour, ISpellCaster
     #region ISpellCaster
 
     public void StartCasting()
-    { }
+    {
+        // Can't cast if the element queue is null or if it has no elements queued up, so we bail out with an early return.
+        if (this.elementQueue == null || this.elementQueue.Count <= 0)
+            return;
+
+        // Update isCasting status.
+        this.isCasting = true;
+
+        HandleStartCasting();
+
+        // After casting the spell, clear the element queue. The element queue is cleared "as soon as the casting starts" (kinda), not after it is finished, so during
+        // sustained casting the player will already see the queue as empty.
+        this.elementQueue.Clear();
+    }
     public void StopCasting()
-    { }
+    {
+        this.isCasting = false;
+        HandleStopCasting(); // Here, specific impls for sustained spells such as beam spells will handle cleaning up their own spawned spells when they are no longer needed.
+    }
     public bool GetIsCasting()
-    { }
+    {
+        return this.isCasting;
+    }
 
     public void SetElementQueue(ElementQueue queue)
-    { }
+    {
+        this.elementQueue = queue;
+    }
     public ElementQueue GetElementQueue()
-    { }
+    {
+        return this.elementQueue;
+    }
 
     public void SetElements(Element[] elements)
-    { }
+    {
+        this.elementQueue.Add(elements); // Add the elements within the array one by one to the element queue to make sure that combinations are handled properly.
+    }
     public Element[] GetElements()
-    { }
+    {
+        return this.elementQueue.Elements;
+    }
     public void AddElement(Element element)
     { }
 
+    /*
     public void SetForm(Form form)
-    { }
+    {
+        this.form = form;
+    }
     public Form GetForm()
-    { }
+    {
+        return this.form;
+    }
+    */
 
     public void SetCastDuration(float time)
-    { }
+    {
+        this.castDuration = time;
+    }
     public float GetCastDuration()
-    { }
+    {
+        return this.castDuration;
+    }
+
+    #endregion
+
+    #region ISpellCaster - Virtual
+
+    // NOTE : These are some shitty default implementations that should never be called, as each specific type should call their own specific impl,
+    // but here they are anyway, so fuck it lol.
+    protected virtual void HandleStartCasting()
+    {
+        DebugManager.Instance?.Log("SpellCasterBaseController::HandleStartCasting()");
+    }
+    protected virtual void HandleStopCasting()
+    {
+        DebugManager.Instance?.Log("SpellCasterBaseController::HandleStopCasting()");
+    }
 
     #endregion
 }
