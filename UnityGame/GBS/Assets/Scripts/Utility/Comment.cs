9a5a350613d1f5dc6f7bdf5fb91ae9ac584be34f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-15 17:34:20 +0100

    Created comment component
    A simple component whose purpose is to contain comments within levels and scenes. This is because not all logic can be located within the code, so scene comments can come in handy, tho they will be shipped in the final build, and that's a waste of space.

---
+++ UnityGame/GBS/Assets/Scripts/Utility/Comment.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comment : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private string comment;
+
+    #endregion
+}
