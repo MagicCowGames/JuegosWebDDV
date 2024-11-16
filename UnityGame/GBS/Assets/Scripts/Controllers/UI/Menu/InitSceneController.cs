3085e00ada3298a60be2a1a84c94a4d5cb8275d7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-16 18:06:33 +0100

    Moved initialization code from DummyManager to InitSceneController
    The screen orientation adjustment code has been moved from the DummyManager to the more logical init code within the InitSceneController.
    
    The Init scene controller now has a macro that checks if the code is being compiled in editor or on a final build, making sure to always disable debug mode by default when starting the game. This way, we can prevent issues where we forget to disable the debug mode on release builds.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/InitSceneController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/InitSceneController.cs
@@ -13,10 +13,8 @@ public class InitSceneController : UIController
 
     void Start()
     {
-        // NOTE : You don't have to worry about the manager instance not existing when calling this on the init scene, because we're calling it from Start(),
-        // and the manager instance is created on Awake(), which means that the order in which the object calls are done by Unity does not matter, since
-        // all of the Awake() stage calls will take place before all of the Start() calls.
-        SceneLoadingManager.Instance?.LoadSceneMainMenu();
+        Init();
+        LoadMainMenu();
     }
 
     void Update()
@@ -30,5 +28,38 @@ public class InitSceneController : UIController
     #endregion
 
     #region PrivateMethods
-    #endregion
+
+    private void Init()
+    {
+        #region Settings - Debug
+        // NOTE : This part of the code that is dedicated to the DebugManager's debug enabled mode should always work since, unlike other systems, the DebugManager
+        // is initialized on Awake(), and this Init() method runs on Start().
+#if UNITY_EDITOR
+        // If compiling the editor build, always set debug to true.
+        DebugManager.Instance?.SetDebugEnabled(true);
+#else
+        // If compiling the release build, always set debug to false by default.
+        // Just to make sure that I don't forget to set the debug to false at some point in the inspector panel before making a release build.
+        DebugManager.Instance?.SetDebugEnabled(false);
+#endif
+        #endregion
+
+        #region Settings - Screen
+
+        // Set the screen orientation to "landscape" for mobile devices.
+        // This gives an error on web when running on a PC, but it doesn't matter cause it just does nothing so that's fine for now.
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+        #endregion
+    }
+
+    private void LoadMainMenu()
+    {
+        // NOTE : You don't have to worry about the manager instance not existing when calling this on the init scene, because we're calling it from Start(),
+        // and the manager instance is created on Awake(), which means that the order in which the object calls are done by Unity does not matter, since
+        // all of the Awake() stage calls will take place before all of the Start() calls.
+        SceneLoadingManager.Instance?.LoadSceneMainMenu();
+    }
+
+#endregion
 }
--- UnityGame/GBS/Assets/Scripts/Managers/DummyManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/DummyManager.cs
@@ -6,9 +6,7 @@ public class DummyManager : SingletonPersistent<DummyManager>
 {
     void Start()
     {
-        // This is not where this should be, but idk what class I should put global config in so here it goes lol...
-        // This gives an error on web when running on a PC, but it doesn't matter cause it just does nothing so that's fine for now.
-        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        
     }
 
     void Update()
