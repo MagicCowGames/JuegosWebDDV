363e6621d5472d0dbdbbbc18ce9a4209d2e6040a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-18 16:05:16 +0100

    Added explanatory comment to DummyManager

--- UnityGame/GBS/Assets/Scripts/Managers/DummyManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/DummyManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Not to be confused with the Dummy NPC Controller. These do not have anything to do with eachother.
 public class DummyManager : SingletonPersistent<DummyManager>
 {
     void Start()
