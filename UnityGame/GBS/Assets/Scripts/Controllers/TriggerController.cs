717c82b2d12b6233088fbcbcf3497f77ae03c086
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-18 15:51:54 +0100

    Modified TriggerController to use a single event and boolean config
    The old system would force trigger users to specify multiple times the same event on different UnityEvent handlers on the inspector panel if a trigger was capable of triggering events on enter, on exit and on stay.
    
    This made it complicated to configure scenarios where a trigger would need to have different behaviour and trigger counts related to each event. For example, a trigger that has a count N on enter, a different coutnt M on exit and an infinite count on stay. This would have been impossible to configure with the old system.
    
    With the new system, a single gameobject can have more than one trigger component, each with a different event configured to run on multiple events or a single event or none at all (these types of triggers could be used exclussively through code by being called with the Trigger() method and adding events with OnTrigger += code;), each with their own name, count and specific config and events.
    
    This makes the new system more flexible and capable of building more complex systems and structures within the level editor.

--- UnityGame/GBS/Assets/Scripts/Controllers/TriggerController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/TriggerController.cs
@@ -4,6 +4,13 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
+// NOTE : The old implementation used to have a separate event handler for each type of event (on area enter,
+// on area exit and on area stay).
+// This is no longer the case. Triggers now only have one single type of event, and boolean checkboxes to determine
+// whether the events are triggered on enter, on exit or on stay.
+// If you need more than one type of trigger, you can either have 2 separate gameobjects or have them attached to the same
+// gameobject with more than one trigger component.
+
 // NOTE : For now, only the player can trigger these. We will make this configurable in the future by adding entity controller scripts which will be gotten on trigger
 // and used to determine what type of entity has collided with the trigger.
 public class TriggerController : MonoBehaviour
@@ -12,14 +19,20 @@ public class TriggerController : MonoBehaviour
 
     [Header("Trigger Properties")]
     [SerializeField] private string triggerName = "DefaultTrigger"; // TODO : Implement a system with a static list on TriggerController where all spawned triggers are added. That way, we can access them / look them up by name. This would mostly be useless unless we allow custom level creation or something like that.
-    [SerializeField] private int triggerCount = 1; // Number of times the trigger can be triggered.
+    [SerializeField] private int count = 1; // Number of times the trigger can be triggered.
     [SerializeField] private bool infinite = true; // Determines if the trigger can be triggered infinite times.
 
     [Header("Trigger Events")]
-    [SerializeField] private UnityEvent OnTrigger; // This one runs when the trigger is activated through some script or another trigger. Will mostly go unused. Could also be renamed to OnTriggerActivated or something like that, but that could be confusing, idk. Maybe should add a trigger always callback that will be triggered from all of the other ones?
-    [SerializeField] private UnityEvent OnTriggerAreaEnter; // This one runs when an entity enters the trigger's area
-    [SerializeField] private UnityEvent OnTriggerAreaExit; // This one runs when an entity exits the trigger's area
-    [SerializeField] private UnityEvent OnTriggerAreaStay; // This one runs when an entity stays on the trigger's area
+    [SerializeField] private UnityEvent onTrigger; // This one runs when the trigger is activated through some script or another trigger. Will mostly go unused. Could also be renamed to OnTriggerActivated or something like that, but that could be confusing, idk. Maybe should add a trigger always callback that will be triggered from all of the other ones?
+    [SerializeField] private bool triggerOnAreaEnter;
+    [SerializeField] private bool triggerOnAreaExit;
+    [SerializeField] private bool triggerOnAreaStay;
+
+    public UnityEvent OnTrigger { get { return this.onTrigger; } set { this.onTrigger = value; } }
+
+    // [SerializeField] private UnityEvent OnTriggerAreaEnter; // This one runs when an entity enters the trigger's area
+    // [SerializeField] private UnityEvent OnTriggerAreaExit; // This one runs when an entity exits the trigger's area
+    // [SerializeField] private UnityEvent OnTriggerAreaStay; // This one runs when an entity stays on the trigger's area
 
     // TODO : Add bools or bitmask to determine what type of entities can activate this trigger.
 
@@ -43,7 +56,11 @@ public class TriggerController : MonoBehaviour
 
     public void Trigger()
     {
-        this.OnTrigger?.Invoke();
+        if (this.infinite || this.count > 0)
+        {
+            this.onTrigger?.Invoke();
+            this.count -= 1;
+        }
     }
 
     #endregion
@@ -69,51 +86,26 @@ public class TriggerController : MonoBehaviour
         return CanTrigger(collider.gameObject);
     }
 
-    private void TryActivateTriggerEvent(GameObject other, UnityEvent triggerEvent)
-    {
-        // If the trigger has been exhausted, then we don't even try it anymore and just bail.
-        if (!(this.infinite || this.triggerCount > 0))
-            return;
-
-        // If the input GameObject can't activate this trigger, then we return.
-        if (!CanTrigger(other))
-            return;
-
-        // If no listeners are registered to the event, then we return.
-        // Otherwise, we would consume the trigger count without the trigger actually doing anything.
-        if (triggerEvent.GetPersistentEventCount() <= 0)
-            return;
-
-        // Activate the trigger event.
-        triggerEvent?.Invoke();
-
-        // Decrease the trigger
-        if(!this.infinite)
-            --this.triggerCount;
-    }
-
-    private void TryActivateTriggerEvent(Collider collider, UnityEvent triggerEvent)
-    {
-        TryActivateTriggerEvent(collider.gameObject, triggerEvent);
-    }
-
     #endregion
 
     #region Collisions
 
     void OnTriggerEnter(Collider other)
     {
-        TryActivateTriggerEvent(other, this.OnTriggerAreaEnter);
+        if (this.triggerOnAreaEnter && CanTrigger(other))
+            Trigger();
     }
 
     void OnTriggerExit(Collider other)
     {
-        TryActivateTriggerEvent(other, this.OnTriggerAreaExit);
+        if(this.triggerOnAreaExit && CanTrigger(other))
+            Trigger();
     }
 
     void OnTriggerStay(Collider other)
     {
-        TryActivateTriggerEvent(other, this.OnTriggerAreaStay);
+        if(this.triggerOnAreaStay && CanTrigger(other))
+            Trigger();
     }
 
     #endregion
