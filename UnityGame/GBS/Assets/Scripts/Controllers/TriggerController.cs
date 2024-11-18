871afe7e0e3abb263a4134cfe44a5b472fbcbad7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-18 15:16:52 +0100

    Updated TriggerController to use a trigger count value
    The trigger count value allows the user to specify how many times the trigger can be activated.
    
    If the value N is positive, then the trigger will activate N times.
    
    If the value N is 0, then the trigger will no longer be triggerable.
    
    If the value N is negative, then the trigger will be capable of being triggered infinite times.

--- UnityGame/GBS/Assets/Scripts/Controllers/TriggerController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/TriggerController.cs
@@ -10,7 +10,11 @@ public class TriggerController : MonoBehaviour
 {
     #region Variables
 
-    // [SerializeField] private string triggerName;
+    [Header("Trigger Properties")]
+    [SerializeField] private string triggerName; // TODO : Implement a system with a static list on TriggerController where all spawned triggers are added. That way, we can access them / look them up by name. This would mostly be useless unless we allow custom level creation or something like that.
+    [SerializeField] private int triggerCount; // Number of times the trigger can be triggered. If it is a negative number, it means infinite times.
+
+    [Header("Trigger Events")]
     [SerializeField] private UnityEvent OnTrigger; // This one runs when the trigger is activated through some script or another trigger. Will mostly go unused. Could also be renamed to OnTriggerActivated or something like that, but that could be confusing, idk. Maybe should add a trigger always callback that will be triggered from all of the other ones?
     [SerializeField] private UnityEvent OnTriggerAreaEnter; // This one runs when an entity enters the trigger's area
     [SerializeField] private UnityEvent OnTriggerAreaExit; // This one runs when an entity exits the trigger's area
@@ -52,6 +56,7 @@ public class TriggerController : MonoBehaviour
         var player = obj.GetComponent<PlayerController>();
         // TODO : Add support for other types of entities appart from the player by adding checks for other controller components
         bool ans =
+            this.triggerCount > 0 &&
             player != null // || whatever != null || etc...
             ;
         return ans;
@@ -62,26 +67,39 @@ public class TriggerController : MonoBehaviour
         return CanTrigger(collider.gameObject);
     }
 
+    private void ActivateTriggerEvent(UnityEvent triggerEvent)
+    {
+        // Activate the trigger event.
+        triggerEvent?.Invoke();
+
+        // Only decrease the trigger count if it is positive.
+        // If it is 0, it stays 0.
+        // If it is a negative value, it stays as said negative value. That way, we prevent the value from
+        // potentially underflowing and breaking triggers that are set to be infinite.
+        if (this.triggerCount > 0)
+            --this.triggerCount;
+    }
+
     #endregion
 
     #region Collisions
 
     void OnTriggerEnter(Collider other)
     {
-        if(CanTrigger(other))
-            OnTriggerAreaEnter?.Invoke();
+        if (CanTrigger(other))
+            ActivateTriggerEvent(this.OnTriggerAreaEnter);
     }
 
     void OnTriggerExit(Collider other)
     {
-        if(CanTrigger(other))
-            OnTriggerAreaExit?.Invoke();
+        if (CanTrigger(other))
+            ActivateTriggerEvent(this.OnTriggerAreaExit);
     }
 
     void OnTriggerStay(Collider other)
     {
-        if(CanTrigger(other))
-            OnTriggerAreaStay?.Invoke();
+        if (CanTrigger(other))
+            ActivateTriggerEvent(this.OnTriggerAreaStay);
     }
 
     #endregion
