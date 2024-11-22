97f3acc7590d8e679eb0fc67dd43fc2c5751ef0f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-22 04:21:33 +0100

    Fully implemented beam collision and splitting system
    I would make multiple commits explaining how this works in greater depth, but I think that the comments are more than enough. It took me quite a long time to achieve this system without any bugs... in short, I had to implement my own collision system of sorts since Unity's seems to be extremely flawed for per-frame collision detection.

--- UnityGame/GBS/Assets/Scripts/Controllers/Magic/Spells/SpellBeamController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Magic/Spells/SpellBeamController.cs
@@ -33,7 +33,9 @@ public class SpellBeamController : SpellBaseController
     public SpellBeamController ParentBeamA { get; set; }
     public SpellBeamController ParentBeamB { get; set; }
 
-    public bool IsChild { get; set; }
+    private bool isChildOwner;
+
+    public bool IsChild { get; private set; }
 
     #endregion
 
@@ -47,6 +49,7 @@ public class SpellBeamController : SpellBaseController
         this.ParentBeamA = null;
         this.ParentBeamB = null;
         this.IsChild = false;
+        this.isChildOwner = false;
     }
 
     void Start()
@@ -134,7 +137,12 @@ public class SpellBeamController : SpellBaseController
         this.capsuleCollider.center = new Vector3(0.0f, 0.0f, this.Length / 2.0f);
 
         // Check if this beam is a child and must destroy themselves
-        if (this.IsChild && (this.ParentBeamA.ChildBeam == null || this.ParentBeamB.ChildBeam == null))
+        if (
+            this.IsChild &&
+            (
+                (this.ParentBeamA == null || this.ParentBeamB == null)
+            )
+        )
         {
             Destroy(this.gameObject);
         }
@@ -149,57 +157,86 @@ public class SpellBeamController : SpellBaseController
         // If the beam has hit, handle hit logic
         if (hasHit)
         {
-            // Update hit point and max beam distance
-            this.TargetPoint = hit.point; // Set the target point of the beam to the hit point.
-            this.currentMaxDistance = hit.distance; // Reset the current max distance to the distance between the origin point and the hit point so that the beam wont grow instantly when moving between surfaces at different distances
+            // Reset the current max distance to the distance between the origin point and the hit point so that the beam wont grow instantly
+            // when moving between surfaces at different distances
+            this.currentMaxDistance = hit.distance;
 
-            // Check if the hit surface is another beam
+            // Check if the hit surface is another beam and set this.OtherBeam to the returned value.
             this.OtherBeam = hit.collider.gameObject.GetComponent<SpellBeamController>();
+        }
+        else
+        {
+            // If there is no hit, reset the this.OtherBeam reference.
+            this.OtherBeam = null;
+        }
 
-            // If it is not a beam, then handle collision with a regular surface
-            if (this.OtherBeam == null)
+        // Check if we are in contact with another beam
+        // NOTE : This is a fucking hack to deal with Unity's shitty collision system... it should not exist, but here we are...
+        bool isContactingBeam = this.OtherBeam != null && (hasHit || (Vector3.Distance(this.TargetPoint, this.OtherBeam.TargetPoint) < 1.0f));
+
+        // Update the max distance by setting it to the min value between the current max distance and the length of the other beam.
+        if (isContactingBeam)
+        {
+            this.currentMaxDistance = Mathf.Min(this.currentMaxDistance, Vector3.Distance(this.OriginPoint, this.OtherBeam.TargetPoint)) + 0.5f;
+        }
+
+        // Set the target point to the max distance point.
+        this.TargetPoint = this.OriginPoint + this.transform.forward * this.currentMaxDistance;
+
+
+        // Handle collisions with other beams and child beam spawning
+        if (isContactingBeam)
+        {
+            // Update the other beam's OtherBeam reference to this beam.
+            // If this beam was colliding with another beam and now collides with this one, it means that this beam is colliding at an earlier point along
+            // the other beam's length, so it will be shortened and we don't need to do anything else to update the logic.
+            this.OtherBeam.OtherBeam = this;
+
+            // Handle the child beam logic
+            // If both beams don't have a child beam, we spawn it and claim ownership (whoever comes first becomes the "child owner")
+            if (this.ChildBeam == null && this.OtherBeam.ChildBeam == null)
             {
-                // Do Nothing
+                this.isChildOwner = true;
+                this.OtherBeam.isChildOwner = false;
+                this.ChildBeam = ObjectSpawner.Spawn(this.beamPrefab, this.TargetPoint).GetComponent<SpellBeamController>();
+                this.ChildBeam.IsChild = true;
+                this.ChildBeam.ParentBeamA = this;
+                this.ChildBeam.ParentBeamB = this.OtherBeam;
+                this.OtherBeam.ChildBeam = this.ChildBeam;
             }
-            // If it is a beam, then handle collision with another beam
+            // If either of the beams has a child, then we handle the child beam logic, but only if we are the child owner.
             else
             {
-                // Update the other beam's OtherBeam reference to this beam.
-                // If this beam was colliding with another beam and now collides with this one, it means that this beam is colliding at an earlier point along
-                // the other beam's length, so it will be shortened and we don't need to do anything else to update the logic.
-                this.OtherBeam.OtherBeam = this;
-
-                // First, shorten both beams to the length of the middle point between their collisions
-                this.OtherBeam.currentMaxDistance = Vector3.Distance(this.OtherBeam.OriginPoint, hit.point);
-
-                // Then, handle the child beam logic
-                if (this.ChildBeam == null && this.OtherBeam.ChildBeam == null) // If both beams don't have a child beam, we spawn it.
+                if (this.isChildOwner)
                 {
-                    this.ChildBeam = ObjectSpawner.Spawn(this.beamPrefab, this.TargetPoint).GetComponent<SpellBeamController>();
-                    this.ChildBeam.IsChild = true;
-                    this.ChildBeam.ParentBeamA = this;
-                    this.ChildBeam.ParentBeamB = this.OtherBeam;
+                    // Remind daddy to come back home with the milk...
                     this.OtherBeam.ChildBeam = this.ChildBeam;
-                }
-                else
-                if (this.ChildBeam != null) // If the child beam is not null for this beam, then we handle it. Otherwise, it will be handled by the other beam.
-                {
+
                     // Set the child beam position to the middle point between the target points of both beams.
                     this.ChildBeam.transform.position = (this.TargetPoint + this.OtherBeam.TargetPoint) / 2.0f;
 
                     // Set the child beam rotation to the average between the rotations of the parent beams.
                     this.ChildBeam.transform.rotation = Quaternion.Lerp(this.transform.rotation, this.OtherBeam.transform.rotation, 0.5f);
-                }
-                else
-                {
-                    this.ChildBeam = this.OtherBeam.ChildBeam; // Update our child beam reference if the other beam's child is not null but ours is.
+
+                    // Remind the child beam who's daddy!
+                    this.ChildBeam.ParentBeamA = this;
+                    this.ChildBeam.ParentBeamB = this.OtherBeam;
                 }
             }
         }
         else
         {
-            this.TargetPoint = this.OriginPoint + this.transform.forward * this.currentMaxDistance; // Set the target point to the max distance point.
-            
+            this.OtherBeam = null;
+            if (!this.isChildOwner)
+                this.ChildBeam = null;
+        }
+
+        if (this.isChildOwner && this.ChildBeam != null && this.OtherBeam == null)
+        {
+            this.ChildBeam.ParentBeamA = null;
+            this.ChildBeam.ParentBeamB = null;
+            this.ChildBeam = null;
+            this.isChildOwner = false;
         }
     }
 
