46b3b7661f45fdd98b4c82b99bc7c6bf12f962a5
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-23 21:45:41 +0100

    Moved AccountMenuController init code to Awake()
    This prevents the menu from appearing for one frame before the correct user account menu when logged in.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Account/AccountMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Account/AccountMenuController.cs
@@ -8,11 +8,21 @@ public class AccountMenuController : MonoBehaviour
     #endregion
 
     #region MonoBehaviour
-    
-    void Start()
+
+    void Awake()
     {
+        // If the user is logged in, then go to the user's account info menu.
+        // Otherwise, stay on the account select menu (the one with the register and login options).
+        #region Comment
+
         // If the user is logged in, then load the "Account control panel"-like UI menu scene, where all of the account info and actions are displayed.
-        // Note that this happens as soon as we enter this scene if we're logged in, so we should not see this scene appear on screen, or just a few frames at most...
+        // Note that this happens as soon as we enter this scene if we're logged in, and since the code runs on Awake() rather than Start(), we don't see
+        // this current scene appear on screen, not even by 1 frame, so it feels smooth and doesn't have a jarring transition (this used to be a problem before
+        // because this code was ran on Start() rather than on Awake(), so we had 1 frame of seeing this menu and then we got popped into the right one... that also
+        // came with the potential problem of an user timing a button press really well and pressing the login or register buttons during that one frame, which would
+        // not break anything, but it would be kind of funny and weird...)
+
+        #endregion
         if (AccountManager.Instance != null && AccountManager.Instance.IsLoggedIn)
         {
             DebugManager.Instance?.Log("User is logged in; Loading user account settings menu.");
@@ -22,6 +32,11 @@ public class AccountMenuController : MonoBehaviour
         {
             DebugManager.Instance?.Log("User is not logged in; Loading default account menu.");
         }
+    }
+
+    void Start()
+    {
+        
     }
 
     void Update()
