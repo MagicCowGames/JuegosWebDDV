fbf75529e469d3f4d0495a8c4bf12479a12aa0e2
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-23 16:12:37 +0100

    Created ScoreboardMenuController script
    Controller script for the scoreboard menu scene

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/ScoreboardMenuController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreboardMenuController : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void Button_Return()
+    {
+        SceneLoadingManager.Instance?.LoadSceneMainMenu();
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
