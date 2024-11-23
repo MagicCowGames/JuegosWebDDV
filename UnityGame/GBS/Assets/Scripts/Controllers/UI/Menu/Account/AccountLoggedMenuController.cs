d62596f27850b047435ca2542d8f075f230c8dd8
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-23 20:35:48 +0100

    Modified logged menu controller to update texts to display username
    The account text on the logged account menu now displays the username.
    
    TODO : Maybe the name of this menu scene and controller should be modified to AccountUserMenu rather than AccountLoggedMenu? it would mean the same and make a bit more sense tbh.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Account/AccountLoggedMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Account/AccountLoggedMenuController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class AccountLoggedMenuController : MonoBehaviour
+public class AccountLoggedMenuController : UIController
 {
     #region Variables
+
+    [Header("Account Logged Menu Controller")]
+    [SerializeField] private TMP_Text[] usernameTexts;
+
     #endregion
 
     #region MonoBehaviour
 
     void Start()
     {
-        
+        UpdateUsernameTexts();
     }
 
     void Update()
@@ -31,5 +36,13 @@ public class AccountLoggedMenuController : MonoBehaviour
     #endregion
 
     #region PrivateMethods
+
+    private void UpdateUsernameTexts()
+    {
+        foreach (var text in this.usernameTexts)
+            if (text != null)
+                text.text = AccountManager.Instance.Account.name;
+    }
+
     #endregion
 }
