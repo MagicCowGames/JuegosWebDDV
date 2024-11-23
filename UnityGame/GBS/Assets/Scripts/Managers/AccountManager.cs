81772e935a8a96802d98e884b08e0a0cbd112ca3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-23 08:52:11 +0100

    Added login handling to AccountManager

--- UnityGame/GBS/Assets/Scripts/Managers/AccountManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/AccountManager.cs
@@ -37,8 +37,9 @@ public class AccountManager : SingletonPersistent<AccountManager>
 
     // NOTE: All of the functions here can stand on their own without any custom callbacks, but an overload with a callbacks parameter exists to allow the
     // callers to define their own callbacks in case they want something else to happen on the call site.
+
     // TODO: Maybe the code for scene loading should be moved to each scene rather than it being here, since logging in could happen through a command
-    // or on other scenes, maybe even during init by using cookies, who knows!
+    // or on other scenes, maybe even during init by using cookies, who knows! For now, it'll stay as it is until it needs to be changed...
 
     public void RegisterAccount(string name, string password)
     {
@@ -82,8 +83,44 @@ public class AccountManager : SingletonPersistent<AccountManager>
         ConnectionManager.Instance.MakeRequest("GET", ConnectionManager.Instance.ServerAddress.http, $"/users/add/{name}/{password}", callbacks);
     }
 
+    public void AccessAccount(string name, string password)
+    {
+        var callbacks = new ConnectionManager.RequestCallbacks();
+        AccessAccount(name, password, callbacks);
+    }
+
     public void AccessAccount(string name, string password, ConnectionManager.RequestCallbacks callbacks)
     {
+        DebugManager.Instance?.Log("Submitting Request to Log into Account...");
+        var popUp = UIManager.Instance?.GetPopUpUIController();
+
+        if (name.Length <= 0)
+        {
+            popUp.Open("loc_error_validation_title", "loc_error_validation_message_name");
+            return;
+        }
+
+        if (password.Length <= 0)
+        {
+            popUp.Open("loc_error_validation_title", "loc_error_validation_message_password");
+            return;
+        }
+
+        callbacks.OnSuccess += (ans) => {
+            DebugManager.Instance?.Log($"Successfully logged into the account : {ans}");
+            this.UserAccount = JsonUtility.FromJson<Account>(ans);
+            this.UserAccount.isLoggedIn = true;
+            SceneLoadingManager.Instance?.LoadSceneAccount();
+        };
+        callbacks.OnError += (err) => {
+            DebugManager.Instance?.Log($"Could not log into the account : {err}");
+            popUp.Open("loc_error_login_title", "loc_error_login_message");
+        };
+        callbacks.OnConnectionError += () => {
+            DebugManager.Instance?.Log("Connection Error");
+            popUp.Open("loc_error_connection_title", "loc_error_connection_message");
+        };
+
         ConnectionManager.Instance.MakeRequest("GET", ConnectionManager.Instance.ServerAddress.http, $"/users/login/{name}/{password}", callbacks);
     }
 
