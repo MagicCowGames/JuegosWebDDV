edda7c635d245e35664a00270b0220fe1d02770f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-23 04:26:20 +0100

    Added required variables to button UI controller

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Generic/ButtonUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Generic/ButtonUIController.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ButtonUIController : MonoBehaviour
 {
     #region Variables
+
+    [Header("Components - Button")]
+    [SerializeField] private Button button;
+    [SerializeField] private Image buttonShadow;
+
+    [Header("Components - Text")]
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private TMP_Text textShadow;
+
+    [Header("Settings - Button")]
+    [SerializeField] private UnityEvent OnClick;
+    [SerializeField] private bool buttonHasShadow;
+
+    [Header("Settings - Text")]
+    [SerializeField] private string localizationString;
+    [SerializeField] private bool textHasShadow;
+
     #endregion
 
     #region MonoBehaviour
