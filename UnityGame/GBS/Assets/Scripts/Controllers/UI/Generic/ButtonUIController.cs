e84bb004f0b919ca97239e0650425578927884ea
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-23 04:49:00 +0100

    Added button sprite handling to button controller

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Generic/ButtonUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Generic/ButtonUIController.cs
@@ -11,10 +11,24 @@ using UnityEngine.Events;
 [ExecuteInEditMode]
 public class ButtonUIController : MonoBehaviour, IComponentValidator
 {
+    #region Enums
+
+    public enum ButtonType
+    {
+        Long = 0,
+        Short
+    }
+
+    #endregion
+
     #region Variables
+
+    // TODO : Maybe, for easier handling, separate all of the component getting logic into some sort of internal logic component or whatever, and
+    // have another component for user facing configuration.
 
     [Header("Components - Button")]
     [SerializeField] private Button button;
+    [SerializeField] private Image buttonImage;
     [SerializeField] private Image buttonShadow;
 
     [Header("Components - Text")]
@@ -22,9 +36,15 @@ public class ButtonUIController : MonoBehaviour, IComponentValidator
     [SerializeField] private TMP_Text textShadow;
     [SerializeField] private LocalizedTextController localizedTextController;
 
+    // TODO : Move this to some sort of global sprite manager so that we don't have a gazillion copies of this data lying around.
+    [Header("Data - Button")]
+    [SerializeField] private Sprite spriteButtonShort;
+    [SerializeField] private Sprite spriteButtonLong;
+
     [Header("Settings - Button")]
     [SerializeField] private Button.ButtonClickedEvent onClick;
     [SerializeField] private bool buttonHasShadow;
+    [SerializeField] private ButtonType buttonType;
 
     [Header("Settings - Text")]
     [SerializeField] private string localizationString;
@@ -74,13 +94,27 @@ public class ButtonUIController : MonoBehaviour, IComponentValidator
     private void UpdateButton()
     {
         this.button.onClick = this.onClick;
-        this.buttonShadow?.gameObject.SetActive(this.buttonHasShadow);
+        this.buttonShadow.gameObject.SetActive(this.buttonHasShadow);
+
+        Sprite chosen;
+        switch (this.buttonType)
+        {
+            default:
+            case ButtonType.Long:
+                chosen = this.spriteButtonLong;
+                break;
+            case ButtonType.Short:
+                chosen = this.spriteButtonShort;
+                break;
+        }
+        this.buttonImage.sprite = chosen;
+        this.buttonShadow.sprite = chosen;
     }
 
     private void UpdateText()
     {
         this.localizedTextController.LocalizationString = this.localizationString;
-        this.textShadow?.gameObject.SetActive(this.textHasShadow);
+        this.textShadow.gameObject.SetActive(this.textHasShadow);
     }
 
     #endregion
