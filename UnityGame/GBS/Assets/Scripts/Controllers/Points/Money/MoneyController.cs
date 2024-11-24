a39aaac4539642e4fe19fc01c05154aa18c9a2fe
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-24 05:48:14 +0100

    Removed old outdated comments from code

--- UnityGame/GBS/Assets/Scripts/Controllers/Points/Money/MoneyController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Points/Money/MoneyController.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// NOTE : This class would make more sense in multiplayer since each player would hold their own score. Currently it goes unused since we just hook into the
-// GameManager's money and score variables directly and fuck right off...
-
 // And the dumbest class award goes to... Basically this is just a controller script that acts as a "pouch", so that we can use get component to add money to
 // basically any entity at all that can pick up money, including the player.
 public class MoneyController : MonoBehaviour
--- UnityGame/GBS/Assets/Scripts/Controllers/Points/Score/ScoreController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Points/Score/ScoreController.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// NOTE : This class would make more sense in multiplayer since each player would hold their own score. Currently it goes unused since we just hook into the
-// GameManager's money and score variables directly and fuck right off...
-
 // Yet another dumb class award... but hey, at least all this crap goes pretty good with Unity's component getting system...
 // Literally the same as the fucking money controller class.
 public class ScoreController : MonoBehaviour
