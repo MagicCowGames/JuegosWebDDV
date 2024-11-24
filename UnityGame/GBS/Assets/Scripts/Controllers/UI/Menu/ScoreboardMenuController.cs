82d3cf6285703a3ca667e05badf72e317aaf791a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-24 20:22:41 +0100

    Implemented scoreboard fetching and updating
    The scoreboard menu is now updated and displays the top 10 scores.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/ScoreboardMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/ScoreboardMenuController.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class ScoreboardMenuController : MonoBehaviour
 {
     #region Variables
+
+    [SerializeField] private UI_ScoreEntryController[] scoreEntries;
+
     #endregion
 
     #region MonoBehaviour
 
     void Start()
     {
-        
+        FetchEntries();
     }
 
     void Update()
@@ -31,5 +34,43 @@ public class ScoreboardMenuController : MonoBehaviour
     #endregion
 
     #region PrivateMethods
+
+    private void SetEntriesEmpty(string emptyStr = "...")
+    {
+        for (int i = 0; i < this.scoreEntries.Length; ++i)
+        {
+            this.scoreEntries[i].SetData(i + 1, emptyStr, emptyStr);
+        }
+    }
+
+    private void SetEntries(ScoreListDTO scores)
+    {
+        for (int i = 0; i < scores.scores.Length; ++i)
+        {
+            var name = scores.scores[i].name;
+            var score = scores.scores[i].score;
+            this.scoreEntries[i].SetData(i + 1, name, score);
+        }
+    }
+
+    private void FetchEntries()
+    {
+        // Reset the entires on the screen.
+        SetEntriesEmpty("...");
+
+        // Fetch the scores from the server.
+        int len = 10;
+        var callbacks = new ConnectionManager.RequestCallbacks();
+        callbacks.OnSuccess += (ans) => {
+            DebugManager.Instance?.Log($"scores = {ans}");
+            var list = JsonUtility.FromJson<ScoreListDTO>(ans);
+            DebugManager.Instance.Log($"list : {list}, list.scores = {list.scores}");
+            // Set the entries to empty and then fill in however many entries have been returned by the server.
+            SetEntriesEmpty("N/A");
+            SetEntries(list);
+        };
+        ConnectionManager.Instance.MakeRequestToServer("GET", $"/score/descending/slice/0/{len}", callbacks);
+    }
+
     #endregion
 }
