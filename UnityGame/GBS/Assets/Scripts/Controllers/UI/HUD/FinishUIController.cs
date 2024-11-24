7b2ec394f8228bb654bb3556d9b47f4be23ff73f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-24 15:55:50 +0100

    Updated FinishUI to check for component validity

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/FinishUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/FinishUIController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class FinishUIController : UIController
+public class FinishUIController : UIController, IComponentValidator
 {
     #region Variables
 
@@ -22,6 +22,9 @@ public class FinishUIController : UIController
 
     void Update()
     {
+        if (!AllComponentsAreValid())
+            return;
+
         UpdateScoreText();
         UpdateMoneyText();
     }
@@ -41,14 +44,27 @@ public class FinishUIController : UIController
 
     private void UpdateScoreText()
     {
-        if (this.scoreText != null)
-            this.scoreText.text = $"{PlayerDataManager.Instance.GetPlayerScore().Score} pts";
+        this.scoreText.text = $"{PlayerDataManager.Instance.GetPlayerScore().Score} pts";
     }
 
     private void UpdateMoneyText()
     {
-        if (this.moneyText != null)
-            this.moneyText.text = $"${PlayerDataManager.Instance.GetPlayerMoney().Money}";
+        this.moneyText.text = $"${PlayerDataManager.Instance.GetPlayerMoney().Money}";
+    }
+
+    #endregion
+
+    #region IComponentValidator
+
+    public bool AllComponentsAreValid()
+    {
+        return
+            PlayerDataManager.Instance != null &&
+            PlayerDataManager.Instance.GetPlayerMoney() != null &&
+            PlayerDataManager.Instance.GetPlayerScore() != null &&
+            this.moneyText != null &&
+            this.scoreText != null
+            ;
     }
 
     #endregion
