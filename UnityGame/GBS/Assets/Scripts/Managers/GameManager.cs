0dcda288cb30bcecdf0d2d2c53c33f2f60c8e8d0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-24 01:52:00 +0100

    Added money and score variables to GameManager
    Since the game is single player, it's going to move away from using player specific money and score components to using a global access point. This can easily be replaced in the future if I ever get around implementing multiplayer tho.

--- UnityGame/GBS/Assets/Scripts/Managers/GameManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/GameManager.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : Singleton<GameManager>
+public class GameManager : SingletonPersistent<GameManager>
 {
     #region Variables
+
+    public int Score { get; set; }
+    public int Money { get; set; }
+
     #endregion
 
     #region MonoBehaviour
