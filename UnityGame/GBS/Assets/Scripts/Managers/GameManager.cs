86a2e94f9a3edd28822ce53aa0e8cd6bd2e0391a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-24 04:48:58 +0100

    Made GameManager a non persistent singleton
    It has been decided to go back to the previous idea where the game manager exists on each level rather than it being a global system that persists across scenes.
    
    This allows triggers in the scene to directly access game manager functionality. It also allows the game manager's logic to be reset.
    
    Again, as a side note, it should be clear that the game should be coded in a way such that it should be capable of running without a game manager instance being present in the scene.

--- UnityGame/GBS/Assets/Scripts/Managers/GameManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : SingletonPersistent<GameManager>
+public class GameManager : Singleton<GameManager>
 {
     #region Variables
 
