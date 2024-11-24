4a43c5d0f1da9e7cba07f08df4ae6cde96b6a1ec
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-24 05:02:27 +0100

    Added score and money updating code to GameManager
    The game manager now performs requests through the AccountManager to update the values of the score and money on the server side.

--- UnityGame/GBS/Assets/Scripts/Managers/GameManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/GameManager.cs
@@ -30,10 +30,16 @@ public class GameManager : Singleton<GameManager>
     // TODO : You gotta Polish that Finish screen! damn these jokes suck... just fix this shit lol, just make it look good...
     public void FinishGame()
     {
+        // Make the player immortal so that they cannot die during this screen.
         PlayerDataManager.Instance.GetPlayerHealth().HealthMin = 1.0f; // Basically, make the player immortal so that they can't die during the victory screen.
+        
+        // Change UI visibility.
         UIManager.Instance.GetFinishUIController().UI_SetVisible(true); // Display the finish UI
         UIManager.Instance.GetPlayerUIController().UI_SetVisible(false); // Hide the player's UI so taht we don't see the scores twice (they are also displayed on the finish UI).
-        // TODO : Update score
+
+        //Update score and money values on the server
+        AccountManager.Instance.UpdateScore(this.Score);
+        AccountManager.Instance.UpdateMoney(this.Money);
     }
 
     #endregion
