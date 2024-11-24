7161a42fbe3376647d577fe9a84da64910091d4e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-24 01:52:35 +0100

    Moved Money related scripts to Points folder

--- UnityGame/GBS/Assets/Scripts/Controllers/Points/Money/MoneyController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Points/Money/MoneyController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// NOTE : This class would make more sense in multiplayer since each player would hold their own score. Currently it goes unused since we just hook into the
+// GameManager's money and score variables directly and fuck right off...
+
 // And the dumbest class award goes to... Basically this is just a controller script that acts as a "pouch", so that we can use get component to add money to
 // basically any entity at all that can pick up money, including the player.
 public class MoneyController : MonoBehaviour
@@ -12,25 +15,5 @@ public class MoneyController : MonoBehaviour
 
     public int Money { get { return this.money; } set{ this.money = value; } }
 
-    #endregion
-
-    #region MonoBehaviour
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
-    #endregion
-
-    #region PublicMethods
-    #endregion
-
-    #region PrivateMethods
     #endregion
 }
