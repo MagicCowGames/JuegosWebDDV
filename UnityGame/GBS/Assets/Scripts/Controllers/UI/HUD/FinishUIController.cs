2b6c58417269ecad197a9eb8bae6b88eb5d602ac
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-24 15:50:05 +0100

    Updated FinishUIController to handle updating money and score texts

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/FinishUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/FinishUIController.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FinishUIController : UIController
 {
     #region Variables
+
+    [Header("Finish UI Controller")]
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text moneyText;
+
     #endregion
 
     #region MonoBehaviour
@@ -16,7 +22,8 @@ public class FinishUIController : UIController
 
     void Update()
     {
-        
+        UpdateScoreText();
+        UpdateMoneyText();
     }
 
     #endregion
@@ -31,5 +38,18 @@ public class FinishUIController : UIController
     #endregion
 
     #region PrivateMethods
+
+    private void UpdateScoreText()
+    {
+        if (this.scoreText != null)
+            this.scoreText.text = $"{PlayerDataManager.Instance.GetPlayerScore().Score} pts";
+    }
+
+    private void UpdateMoneyText()
+    {
+        if (this.moneyText != null)
+            this.moneyText.text = $"${PlayerDataManager.Instance.GetPlayerMoney().Money}";
+    }
+
     #endregion
 }
