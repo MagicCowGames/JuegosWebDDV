4ee47294e171719c994947a2827fa149d1218c76
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-26 00:37:22 +0100

    Renamed TutorialMenuController to TutorialSelectMenuController
    This script has been renamed like this because it had become too confusing to reason about the naming between the tutorial selection screen and the readable tutorial screen.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Tutorial/TutorialSelectMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Tutorial/TutorialSelectMenuController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TutorialMenuController : MonoBehaviour
+public class TutorialSelectMenuController : MonoBehaviour
 {
     #region Variables
 
