483960b00f46b002a64036cd043cc2d08f3f656e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-26 00:31:01 +0100

    Implemented basic functionality for tutorial menu controller

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/TutorialMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/TutorialMenuController.cs
@@ -19,8 +19,8 @@ public class TutorialMenuController : MonoBehaviour
         // THIS CRAPPY PATCH LOADS STRAIGHT INTO THE TUTORIAL MAP. LOADS ON AWAKE TO PREVENT VISUALIZING THIS SCENE EVEN FOR 1 FRAME.
         // YES, SHITTY IDEA, BUT I DID THIS BECAUSE I'M OUT OF FUCKING TIME AND I CAN'T REIMPLEMENT THE IN-MENU TUTORIAL.
 
-        SoundManager.Instance?.StopMusic();
-        SceneLoadingManager.Instance?.LoadScene("TutorialMap");
+        // SoundManager.Instance?.StopMusic();
+        // SceneLoadingManager.Instance?.LoadScene("TutorialMap");
     }
 
     void Start()
@@ -40,6 +40,16 @@ public class TutorialMenuController : MonoBehaviour
     public void ReturnToMenu()
     {
         SceneLoadingManager.Instance?.LoadSceneMainMenu();
+    }
+
+    public void Button_TutorialMenu()
+    {
+        SceneLoadingManager.Instance?.LoadScene("MenuScene_Tutorial_Pages");
+    }
+
+    public void Button_TutorialLevel()
+    {
+        SceneLoadingManager.Instance?.LoadScene("TutorialMap");
     }
 
     #endregion
