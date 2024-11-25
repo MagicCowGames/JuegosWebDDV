69e65b94de2c588aba70ac9c58bcb1a4349edb6a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-26 00:38:31 +0100

    Created TutorialPagesMenuController script
    This controller script will be in charge of controlling the logic for the readable tutorial menu.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Tutorial/TutorialPagesMenuController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialPagesMenuController : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void ReturnToMenu()
+    {
+        SceneLoadingManager.Instance?.LoadSceneMainMenu();
+    }
+
+    public void Button_TutorialMenu()
+    {
+        SceneLoadingManager.Instance?.LoadScene("MenuScene_Tutorial_Pages");
+    }
+
+    public void Button_TutorialLevel()
+    {
+        SceneLoadingManager.Instance?.LoadScene("TutorialMap");
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
