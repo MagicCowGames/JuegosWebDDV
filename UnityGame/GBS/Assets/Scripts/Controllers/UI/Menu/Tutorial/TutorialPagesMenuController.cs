0fe9aabb24c5dc59146f3773cc456ae022b49b00
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-26 01:09:29 +0100

    Implemented basic page handling for tutorial pages menu controller

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Tutorial/TutorialPagesMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Tutorial/TutorialPagesMenuController.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+// NOTE : This could be reused in the scoreboard or somewhere else to make multi-page menus.
 public class TutorialPagesMenuController : MonoBehaviour
 {
     #region Variables
+
+    [SerializeField] private Button buttonBack;
+    [SerializeField] private Button buttonNext;
+
+    [SerializeField] private GameObject[] pages;
+
+    private int currentPage;
 
     #endregion
 
@@ -13,7 +22,8 @@ public class TutorialPagesMenuController : MonoBehaviour
 
     void Start()
     {
-        
+        this.currentPage = 0;
+        UpdateDisplayedTutorialPage();
     }
 
     void Update()
@@ -31,13 +41,41 @@ public class TutorialPagesMenuController : MonoBehaviour
     }
 
     public void Button_Next()
-    { }
+    {
+        this.currentPage++;
+        UpdateDisplayedTutorialPage();
+    }
 
     public void Button_Previous()
-    { }
+    {
+        this.currentPage--;
+        UpdateDisplayedTutorialPage();
+    }
 
     #endregion
 
     #region PrivateMethods
+
+    private void HideTutorialPages()
+    {
+        foreach (var page in this.pages)
+            page.SetActive(false);
+    }
+
+    private void DisplayTutorialPage(int idx)
+    {
+        HideTutorialPages();
+        this.pages[idx].SetActive(true);
+    }
+
+    private void UpdateDisplayedTutorialPage()
+    {
+        DisplayTutorialPage(this.currentPage);
+
+        this.buttonBack.gameObject.SetActive(this.currentPage > 0);
+        this.buttonNext.gameObject.SetActive(this.currentPage < this.pages.Length - 1);
+
+    }
+
     #endregion
 }
