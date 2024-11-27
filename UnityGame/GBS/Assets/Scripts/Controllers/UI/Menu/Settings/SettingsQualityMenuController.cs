194663f74544616a20c6fcbfa57d05513f09a735
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-27 20:29:31 +0100

    Created and implemented SettingsQualityMenuController
    This controller script is in charge of controlling the logic the quality settings menu screen.
    
    This allows the users to change the quality levels through the settings UI.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsQualityMenuController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsQualityMenuController : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void SetQualityVeryLow() { SetQualityInternal(0); }
+    public void SetQualityLow() { SetQualityInternal(1); }
+    public void SetQualityMedium() { SetQualityInternal(2); }
+    public void SetQualityHigh() { SetQualityInternal(3); }
+    public void SetQualityVeryHigh() { SetQualityInternal(4); }
+    public void SetQualityUltra() { SetQualityInternal(5); }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private void SetQualityInternal(int level)
+    {
+        QualitySettings.SetQualityLevel(level);
+    }
+
+    #endregion
+}
