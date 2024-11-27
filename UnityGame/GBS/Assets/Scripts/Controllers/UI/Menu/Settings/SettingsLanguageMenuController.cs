b9a441152367e7ceb7983704c9a2b6da52eadd9f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-27 20:24:29 +0100

    Created and implemented language menu controller

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsLanguageMenuController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsLanguageMenuController : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    // NOTE : This looks kinda funky, and yes, it should be possible to do this with just 1 function that takes the language as an input, but sadly it is not...
+    // Unity's buttons on click events use UnityEvents, which don't seem to support enum inputs... XD
+
+    public void SetLanguageEnglish()
+    {
+        LanguageSystem.SetLanguage(LanguageSystem.Language.English);
+    }
+
+    public void SetLanguageSpanish()
+    {
+        LanguageSystem.SetLanguage(LanguageSystem.Language.Spanish);
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
