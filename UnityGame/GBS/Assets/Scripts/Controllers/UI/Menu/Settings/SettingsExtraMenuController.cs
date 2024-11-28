8063854079aa05ec5040118a4b2094770453dda0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-28 12:50:17 +0100

    Renamed Settings toggles menu with Settings Extra menu

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsExtraMenuController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsExtraMenuController : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
