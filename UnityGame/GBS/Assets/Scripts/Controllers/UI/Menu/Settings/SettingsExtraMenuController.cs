e7e6b8b23939c2104af55b2c0d39373f10d038d0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-28 12:50:46 +0100

    Added function signatures for extras menu controller

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsExtraMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsExtraMenuController.cs
@@ -23,7 +23,22 @@ public class SettingsExtraMenuController : MonoBehaviour
 
     #region PublicMethods
 
-    
+    public void SetDebugEnabled(bool enabled)
+    { }
+
+    public void SetConsoleEnabled(bool enabled)
+    { }
+
+    public void SetVersionEnabled(bool enabled)
+    { }
+
+    public void SetFpsEnabled(bool enabled)
+    { }
+
+    public void Button_Return()
+    {
+        SceneLoadingManager.Instance?.LoadSceneSettings();
+    }
 
     #endregion
 
