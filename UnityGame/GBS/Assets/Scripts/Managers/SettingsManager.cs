55ec28cfc57e519d81f9d176de8a528304b3cfac
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-29 05:25:46 +0100

    Reworked SettingsManager saving and loading system
    The settings manager class now acts as a DTO handler manager class for settings. It generates an UserSettings class object when needed, collecting the information and settings from the real physical systems of the program and then sends them as JSON to the web server to be stored, acitng as a simple packaging DTO class.
    
    It also has a method to set data from loaded data, which allows the system to modify the values of the real physical systems based on those of an UserSettings object given to it.
    
    These changes make serialization and deserialization code generic enough that it should be easier to modify the systmen in the long run, as well as allowing an easier implementation for local file data management.

--- UnityGame/GBS/Assets/Scripts/Managers/SettingsManager.cs
+++ UnityGame/GBS/Assets/Scripts/Managers/SettingsManager.cs
@@ -14,9 +14,9 @@ public class SettingsManager : SingletonPersistent<SettingsManager>
 {
     #region Variables
 
-    private UserSettings settings;
+    // private UserSettings settings;
 
-    public UserSettings Settings { get { return this.settings; } set { this.settings = value; } }
+    // public UserSettings Settings { get { return this.settings; } set { this.settings = value; } }
 
     #endregion
 
@@ -42,6 +42,7 @@ public class SettingsManager : SingletonPersistent<SettingsManager>
     // Update the settings on all of the systems that make use of them.
     // NOTE : Calling this is kind of a bad idea if you are going to update only one single setting, since internally this forces all settings to update,
     // and that can be kinda heavy for graphics-related settings.
+    /*
     public void UpdateSettings()
     {
         UpdateSettings_Graphics();
@@ -50,53 +51,116 @@ public class SettingsManager : SingletonPersistent<SettingsManager>
         UpdateSettings_Extra();
         UpdateSettings_Cosmetic();
     }
+    */
 
     // Saves the settings (persistence)
     // In local builds, this should save data to the disk (not yet implemented). On WebGL builds, it updates the settings on the web server.
     // This used to be named "UpdateUserAccountSettings()", but SaveSettings() is shorter, faster to type and easier to remember, so yeah lol.
     public void SaveSettings()
     {
+        #region WebGL
+
         // Can't save settings if we're not logged in yet!
         if (!AccountManager.Instance.IsLoggedIn)
             return;
 
+        var settings = GetSettings(); // Construct a settings object from the data within the real/"physical" systems of the game.
         var id = AccountManager.Instance.Account.id;
         var password = AccountManager.Instance.Account.password;
-        var settingsStr = JsonUtility.ToJson(this.Settings);
+        var settingsStr = JsonUtility.ToJson(settings);
 
-        ConnectionManager.Instance.MakeRequestToServer("GET", $"/users/update/settings/{id}/{password}/{settingsStr}");
+        ConnectionManager.Instance.MakeRequestToServer("GET", $"/users/update/settings/{id}/{password}/{settingsStr}"); // TODO : Update this string to be users/set instead of /update for consistency. Update on the serverside too.
+
+        #endregion
+
+        #region Local
+        
+        // TODO : Implement
+
+        #endregion
     }
 
     // TODO : Make a LoadSettings() function and move the settings loading code from the other classes to here. That way, under the hood, this could all be
     // replaced with simpler code to actually just save data to files on the disk when making builds other than WebGL.
+    // NOTE : Yes, I know it's kinda dumb to divide the settings getting into a 2 step thing... but at least it makes the code generic enough to make it possible
+    // to easily implement settings loading for builds that run on local devices rather than WebGL builds, which will come in handy later on...
+    public void LoadSettings()
+    {
+        #region WebGL
+
+        var callbacks = new ConnectionManager.RequestCallbacks();
+        callbacks.OnSuccess += (ans) => {
+            var settings = JsonUtility.FromJson<UserSettings>(ans);
+            SetSettings(settings); // Apply the data within this DTO to the real physical systems of the game by actually loading the settings.
+        };
+        var id = AccountManager.Instance.Account.id;
+        var password = AccountManager.Instance.Account.password;
+        ConnectionManager.Instance.MakeRequestToServer("GET", $"/users/get/settings/{id}/{password}"); // TODO : Adjust this string to fit whatever is implemented on the serverside later on.
+
+        #endregion
+
+        #region Local
+        
+        // TODO : Implement
+
+        #endregion
+    }
 
     #endregion
 
     #region PrivateMethods
 
-    private void UpdateSettings_Graphics()
+    // Gets the real settings data from the real systems that contain the actual variables that contain the data currently being used by the program.
+    // It then stores it within an UserSettings object and returns it.
+    // Basically, a fetching function of sorts that updates the DTO's contents when needed.
+    private UserSettings GetSettings()
     {
-        QualitySettings.SetQualityLevel(this.settings.graphicsSettings.quality);
+        UserSettings settings = new UserSettings();
+
+        settings.graphicsSettings.quality = QualitySettings.GetQualityLevel();
+        settings.languageSettings.language = LanguageSystem.GetLanguage();
+        // settings.soundSettings.etc... // TODO : Implement
+        settings.extraSettings.displayVersion = UIManager.Instance.GetInfoUI().DisplayVersion;
+        settings.extraSettings.displayFps = UIManager.Instance.GetInfoUI().DisplayFPS;
+        settings.extraSettings.consoleEnabled = UIManager.Instance.GetConsoleUI().ConsoleEnabled;
+        // settings.cosmeticSettings.color = etc...;
+
+        return settings;
     }
 
-    private void UpdateSettings_Language()
+    // Update the settings using the data stored within an UserSettings object.
+    private void SetSettings(UserSettings settings)
     {
-        LanguageSystem.SetLanguage(this.settings.languageSettings.language);
+        UpdateSettings_Graphics(settings.graphicsSettings);
+        UpdateSettings_Language(settings.languageSettings);
+        UpdateSettings_Sound(settings.soundSettings);
+        UpdateSettings_Extra(settings.extraSettings);
+        UpdateSettings_Cosmetic(settings.cosmeticSettings);
     }
 
-    private void UpdateSettings_Sound()
+    private void UpdateSettings_Graphics(GraphicsSettings settings)
+    {
+        QualitySettings.SetQualityLevel(settings.quality);
+    }
+
+    private void UpdateSettings_Language(LanguageSettings settings)
+    {
+        LanguageSystem.SetLanguage(settings.language);
+    }
+
+    private void UpdateSettings_Sound(SoundSettings settings)
     {
         // TODO : Implement
     }
 
-    private void UpdateSettings_Extra()
+    private void UpdateSettings_Extra(ExtraSettings settings)
     {
-        UIManager.Instance.GetInfoUI().DisplayFPS = this.settings.extraSettings.displayFps;
-        UIManager.Instance.GetInfoUI().DisplayVersion = this.settings.extraSettings.displayVersion;
-        UIManager.Instance.GetConsoleUI().ConsoleEnabled = this.settings.extraSettings.consoleEnabled;
+        UIManager.Instance.GetInfoUI().DisplayFPS = settings.displayFps;
+        UIManager.Instance.GetInfoUI().DisplayVersion = settings.displayVersion;
+        UIManager.Instance.GetConsoleUI().ConsoleEnabled = settings.consoleEnabled;
     }
 
-    private void UpdateSettings_Cosmetic()
+    private void UpdateSettings_Cosmetic(CosmeticSettings settings)
     {
         // TODO : Implement
     }
