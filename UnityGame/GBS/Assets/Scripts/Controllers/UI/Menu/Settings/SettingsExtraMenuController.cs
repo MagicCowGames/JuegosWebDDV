f5aa03ce335ff6345408a13916f77196b13b2dfe
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-29 02:44:57 +0100

    Added settings toggle handling code to extras settings menu
    The extra settings menu can now handle toggling on and off the version display, the fps display and the console enabled setting.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsExtraMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsExtraMenuController.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsExtraMenuController : MonoBehaviour
 {
     #region Variables
+
+    // [SerializeField] private Toggle debugEnabledToggle;
+    [SerializeField] private Toggle consoleEnabledToggle;
+    [SerializeField] private Toggle versionEnabledToggle;
+    [SerializeField] private Toggle fpsEnabledToggle;
+
     #endregion
 
     #region MonoBehaviour
 
     void Start()
     {
-        
+        this.consoleEnabledToggle.isOn = UIManager.Instance.GetConsoleUI().ConsoleEnabled;
+        this.versionEnabledToggle.isOn = UIManager.Instance.GetInfoUI().DisplayVersion;
+        this.fpsEnabledToggle.isOn = UIManager.Instance.GetInfoUI().DisplayFPS;
     }
 
     void Update()
@@ -23,17 +32,26 @@ public class SettingsExtraMenuController : MonoBehaviour
 
     #region PublicMethods
 
+    /*
     public void SetDebugEnabled(bool enabled)
     { }
+    */
 
     public void SetConsoleEnabled(bool enabled)
-    { }
+    {
+        DebugManager.Instance?.Log($"enabled from button = {enabled}");
+        UIManager.Instance.GetConsoleUI().ConsoleEnabled = enabled;
+    }
 
     public void SetVersionEnabled(bool enabled)
-    { }
+    {
+        UIManager.Instance.GetInfoUI().DisplayVersion = enabled;
+    }
 
     public void SetFpsEnabled(bool enabled)
-    { }
+    {
+        UIManager.Instance.GetInfoUI().DisplayFPS = enabled;
+    }
 
     public void Button_Return()
     {
