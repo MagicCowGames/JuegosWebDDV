eedd0e60c36b5fb8dcc63d04c49db2724e7840c9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-29 04:49:19 +0100

    Created new settings classes for greater categorization
    The new serializable settings classes will make it easier to edit settings and load settings when interacting with the server.
    
    Note that these classes are still just DTOs as of now, and modifying their values does not do anything to affect the real systems.

--- UnityGame/GBS/Assets/Scripts/Data/Settings.cs
+++ UnityGame/GBS/Assets/Scripts/Data/Settings.cs
@@ -5,6 +5,77 @@ using UnityEngine;
 [System.Serializable]
 public class UserSettings
 {
+    public GraphicsSettings graphicsSettings;
+    public LanguageSettings languageSettings;
+    public SoundSettings soundSettings;
+    public ExtraSettings extraSettings;
+    public CosmeticSettings cosmeticSettings;
+
+    public UserSettings()
+    {
+        this.graphicsSettings = new GraphicsSettings();
+        this.languageSettings = new LanguageSettings();
+        this.soundSettings = new SoundSettings();
+        this.extraSettings = new ExtraSettings();
+        this.cosmeticSettings = new CosmeticSettings();
+    }
+}
+
+[System.Serializable]
+public class GraphicsSettings
+{
+    public int quality;
+
+    public GraphicsSettings()
+    {
+        this.quality = 3; // High
+    }
+}
+
+[System.Serializable]
+public class LanguageSettings
+{
+    public LanguageSystem.Language language;
+
+    public LanguageSettings()
+    {
+        this.language = LanguageSystem.Language.English;
+    }
+}
+
+[System.Serializable]
+public class SoundSettings
+{
+    public float volumeMusic;
+    public float volumeVoice;
+    public float volumeEffects;
+
+    public SoundSettings()
+    {
+        this.volumeMusic = 1.0f;
+        this.volumeVoice = 1.0f;
+        this.volumeEffects = 1.0f;
+    }
+}
+
+[System.Serializable]
+public class ExtraSettings
+{
+    public bool consoleEnabled;
+    public bool displayVersion;
+    public bool displayFps;
+
+    public ExtraSettings()
+    {
+        this.consoleEnabled = false;
+        this.displayVersion = true;
+        this.displayFps = false;
+    }
+}
+
+[System.Serializable]
+public class CosmeticSettings
+{
     [System.Serializable]
     public class Color3f
     {
@@ -20,14 +91,11 @@ public class UserSettings
         }
     }
 
-    public LanguageSystem.Language language;
     public Color3f color;
-    public float volume;
+    // Other settings related to the selected outfit ID, etc...
 
-    public UserSettings()
+    public CosmeticSettings()
     {
-        this.language = LanguageSystem.Language.English;
-        this.color = new Color3f(1.0f, 0.0f, 0.0f);
-        this.volume = 1.0f;
+        this.color = new Color3f(0.0f, 0.0f, 0.0f);
     }
 }
