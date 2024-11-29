736152d4a2016267ba283fc5d2d11de98ae72c2c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-11-29 07:04:45 +0100

    Moved init code from Start() to Awake() in SettingsExtraMenuController
    This change prevents the toggle boxes from visibly changing during one frame from their default state to the correct target state.

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsExtraMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsExtraMenuController.cs
@@ -16,11 +16,20 @@ public class SettingsExtraMenuController : MonoBehaviour
 
     #region MonoBehaviour
 
-    void Start()
+    void Awake()
     {
+        // This init code that sets the toggles to the correct initial state takes place on Awake() since we don't want to see the buttons on the possibly wrong
+        // default state for 1 frame before adopting the correct value. Calling this on Awake() makes the update take place before anything is rendered to the
+        // screen, so we always get to see the correct state for the toggles when we open the menu... now, if the user changes stuff through the console, that's
+        // a whole other story we ain't tackled yet lol...
         this.consoleEnabledToggle.isOn = UIManager.Instance.GetConsoleUI().ConsoleEnabled;
         this.versionEnabledToggle.isOn = UIManager.Instance.GetInfoUI().DisplayVersion;
         this.fpsEnabledToggle.isOn = UIManager.Instance.GetInfoUI().DisplayFPS;
+    }
+
+    void Start()
+    {
+        
     }
 
     void Update()
