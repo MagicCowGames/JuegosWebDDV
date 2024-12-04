c04281d5e767c7b364ae555af8873b1bb714c42b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-05 00:26:19 +0100

    Added method for retry button to PauseUIController

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PauseUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/PauseUIController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseUIController : UIController
 {
@@ -26,6 +27,11 @@ public class PauseUIController : UIController
     public void Button_Resume()
     {
         GameUtility.Resume();
+    }
+
+    public void Button_Retry()
+    {
+        SceneLoadingManager.Instance?.ReloadSceneWithTransition();
     }
 
     public void Button_ReturnToMenu()
