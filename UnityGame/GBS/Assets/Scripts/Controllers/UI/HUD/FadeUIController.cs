fdc2ba0c4fc6577b7b912af8dd2f98ee22950b72
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-04 21:52:07 +0100

    Implemented FadeUIController script
    This controller script is in charge of controlling the logic to change the opacity, color and display image for the fading in and out events.
    
    This fading system also allows the user to fade in and out with a custom selected target opacity and fade duration.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/FadeUIController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// NOTE : The Fade UI supports custom images and colors for the fading UI.
+// The system is inspired by the GTA connected mod's fading mechanism.
+public class FadeUIController : UIController
+{
+    #region Variables
+
+    [Header("Menu PopUp Controller")]
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private Image backgroundImage;
+
+    private float targetOpacity;
+    private float lerpSpeed;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        float delta = Time.deltaTime;
+        UpdateFade(delta);
+    }
+
+    #endregion
+
+    #region PublicMethods - Fade In / Out
+
+    public void Fade(float targetOpacity, float duration = 1.5f)
+    {
+        this.targetOpacity = targetOpacity;
+        float distance = targetOpacity - this.backgroundImage.color.a;
+        this.lerpSpeed = distance / duration;
+    }
+
+    public void FadeIn(float duration = 1.5f)
+    {
+        Fade(0.0f, duration);
+    }
+
+    public void FadeOut(float duration = 1.5f)
+    {
+        Fade(1.0f, duration); // Bender es genial, Fundido a negro.
+    }
+
+    #endregion
+
+    #region PublicMethods - Image
+
+    private void SetColor(Color color)
+    {
+        this.backgroundImage.color = color;
+    }
+
+    private void SetImage(Sprite image)
+    {
+        this.backgroundImage.sprite = image;
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private void Init()
+    {
+        this.targetOpacity = 0.0f;
+        this.lerpSpeed = 0.0f;
+    }
+
+    private void UpdateFade(float delta)
+    {
+        this.canvasGroup.alpha = Mathf.Lerp(this.canvasGroup.alpha, this.targetOpacity, delta * this.lerpSpeed);
+    }
+
+    #endregion
+}
