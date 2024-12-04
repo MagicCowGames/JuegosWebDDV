c4ecf360efba986e39de5ea3d4118b64e601a6c2
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-05 00:26:34 +0100

    Refactored DeathUIController to use the new scene reloading method on retry

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/DeathUIController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/HUD/DeathUIController.cs
@@ -24,12 +24,10 @@ public class DeathUIController : UIController
 
     #region PublicMethods
 
-    // NOTE : As of now, it simply reloads the only scene we have.
-    // TODO : Change implementation to get current level name and reload from start rather than hardcoding the level to be loaded.
-    // Maybe even add checkpoint support if you find it to be cool with this game's gameplay.
+    // TODO : Maybe even add checkpoint support if you find it to be cool with this game's gameplay.
     public void Button_Retry()
     {
-        SceneLoadingManager.Instance?.LoadScene(SceneManager.GetActiveScene().name);
+        SceneLoadingManager.Instance?.ReloadSceneWithTransition();
     }
 
     public void Button_ReturnToMenu()
