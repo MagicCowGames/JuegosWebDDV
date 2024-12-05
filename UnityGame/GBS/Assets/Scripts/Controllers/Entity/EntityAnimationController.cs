273670e831e8ee95fab22938457bfe48a3bad744
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-05 18:49:45 +0100

    Added EntityController reference to EntityAnimationController
    TODO : This needs to be cleaned up and just reference the components that it needs to rather than the EntityController itself... that way the entity controller can be a lot cleaner and reference less components.

--- UnityGame/GBS/Assets/Scripts/Controllers/Entity/EntityAnimationController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Entity/EntityAnimationController.cs
@@ -6,18 +6,21 @@ public class EntityAnimationController : MonoBehaviour
 {
     #region Variables
 
+    private EntityController controller;
+
     #endregion
 
     #region MonoBehaviour
 
     void Start()
     {
-        
+        this.controller = GetComponent<EntityController>();
     }
 
     void Update()
     {
-        
+        if (this.controller == null)
+            return;
     }
 
     #endregion
