28dc28bfd98e2a4e981f9ad8fc7600f8335245e3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-06 01:31:18 +0100

    Implemented range mapping function in GameMath

--- UnityGame/GBS/Assets/Scripts/Utility/GameMath.cs
+++ UnityGame/GBS/Assets/Scripts/Utility/GameMath.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public static class GameMath
 {
+    // Calculate the sigmoid
     public static float Sigmoid(float x)
     {
         return 1.0f / (1.0f + Mathf.Exp(-x));
+    }
+
+    // Maps value from an input range to an output range
+    public static float MapValue(float value, float inputMin, float inputMax, float outputMin, float outputMax)
+    {
+        float inputRange = inputMax - inputMin;
+        float outputRange = outputMax - outputMin;
+        return (((value / inputRange) * outputRange) + outputMin);
     }
 }
