915da4292958d83cd55e7c258c0e2e9c00e380a2
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-07 03:48:46 +0100

    Created behaviour controller script for ghost NPCs

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/Behaviours/GhostBehaviourController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostBehaviourController : NPCBehaviourController
+{
+    protected override void InitBehaviour()
+    {
+
+    }
+
+    protected override void UpdateBehaviour(float delta)
+    {
+
+    }
+}
