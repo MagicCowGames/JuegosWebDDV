026184e6c05675b4cecf0ee3a2f9b532d0c95435
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-07 03:30:30 +0100

    Created NPC Behaviour controller script
    This controller script is where the logic for a specific NPC's behaviour is going to be located.
    
    This means that the NPC controller class will simply contain the basic and generic systems for an NPC to be able to move around and follow a target, as well as any other subsystems that the AI will require, but it will NOT implement the FSM or utility systems that it currently does, allowing the NPC Controller to become a generic framework for NPC actions, while NPC Behaviours control the logic of the behaviour itself for each specific type of NPC.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/Behaviours/NPCBehaviourController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class controls the logic for specific behaviours for NPCs.
+// This will, for example, implement the state machines for specific AIs and whatnot.
+// NOTE : This is the base class from which you should inherit if you want to implement behaviours for NPCs.
+// NOTE : Each NPC should have one single behaviour, mixing multiple behaviours is bound to fuck shit up, so that's why we limit it.
+[DisallowMultipleComponent]
+[RequireComponent(typeof(NPCController))]
+public class NPCBehaviourController : MonoBehaviour
+{
+    #region Variables
+
+    protected NPCController npcController;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        this.npcController = GetComponent<NPCController>();
+        // if(this.npcController != null) // Since we're using require component, this check ain't needed anymore.
+        InitBehaviour();
+    }
+
+    void Update()
+    {
+        float delta = Time.deltaTime;
+        // if(this.npcController != null)
+        UpdateBehaviour(delta);
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region ProtectedMethods
+
+    protected virtual void InitBehaviour()
+    {
+
+    }
+
+    protected virtual void UpdateBehaviour(float delta)
+    {
+
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
