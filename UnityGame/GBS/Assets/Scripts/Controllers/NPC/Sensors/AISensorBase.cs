dda1eed0aba4eb4be50fe6e4bf6ea5f0acc2eb0d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-07 17:07:23 +0100

    Modified AISensorBase class to contemplate detection speed
    This change allows the detection system to slowly detect entities / the player with AI sensors over time as well as instantly.
    
    Useful for a semi-stealth system of sorts to prevent all NPCs from aritifically detecting the player instantly.

--- UnityGame/GBS/Assets/Scripts/Controllers/NPC/Sensors/AISensorBase.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/Sensors/AISensorBase.cs
@@ -7,19 +7,21 @@ public class AISensorBase : MonoBehaviour
 {
     #region Variables
 
+    [SerializeField] protected Transform originTransform;
+    [SerializeField] protected float detectionAmount = 2.0f;
     [SerializeField] public bool senseOnEnter = true;
     [SerializeField] public bool senseOnStay = false;
     [SerializeField] public bool senseOnExit = false;
 
-    public Action<GameObject> OnSense;
+    public Action<GameObject, float> OnSense;
 
     #endregion
 
     #region ProtectedMethods
 
-    protected virtual void Sense(GameObject obj)
+    protected virtual void Sense(GameObject obj, float delta, float distance)
     {
-        this.OnSense?.Invoke(obj);
+        this.OnSense?.Invoke(obj, delta * 1.0f / distance); // NOTE : This is an example invokation that the caller could do on their specific implementation.
     }
 
     #endregion
@@ -28,20 +30,20 @@ public class AISensorBase : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if(this.senseOnEnter)
-            this.Sense(other.gameObject);
+        if (this.senseOnEnter)
+            this.Sense(other.gameObject, 1.0f, Vector3.Distance(this.originTransform.position, other.gameObject.transform.position));
     }
 
     void OnTriggerStay(Collider other)
     {
         if (this.senseOnStay)
-            this.Sense(other.gameObject);
+            this.Sense(other.gameObject, Time.deltaTime, Vector3.Distance(this.originTransform.position, other.gameObject.transform.position));
     }
 
     void OnTriggerExit(Collider other)
     {
         if (this.senseOnExit)
-            this.Sense(other.gameObject);
+            this.Sense(other.gameObject, 1.0f, Vector3.Distance(this.originTransform.position, other.gameObject.transform.position));
     }
 
     #endregion
