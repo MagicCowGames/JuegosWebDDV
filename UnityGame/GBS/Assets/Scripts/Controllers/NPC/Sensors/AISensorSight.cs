43381e04839a19b2e034120279b657fb9aef9456
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-07 17:10:05 +0100

    Fixed bug in sight sensor and implemented detection over time system
    There was a bug in the sight sensor detection system that had gone unnoticed until now, where the sensor would simply not detect anything at all. This was caused by thefact that the player's origin point is on its feet, which would cause the detection line trace to always hit the ground first.
    
    This has now been fixed by adding a small tolerance value and casting the ray higher.
    
    The sight system has been modified to make use of the new detection over time system. The sight detection depends on the distance to the player.
    
    The debug drawing system for sight detection has also been improved.

--- UnityGame/GBS/Assets/Scripts/Controllers/NPC/Sensors/AISensorSight.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/NPC/Sensors/AISensorSight.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class AISensorSight : AISensorBase
 {
-    protected override void Sense(GameObject obj)
+    protected override void Sense(GameObject obj, float delta, float distance)
     {
+        // Debug color for line trace
+        Color debugColor;
+
         // Only process the sight hit if the player is within the sight bounding area.
         var player = obj.GetComponent<PlayerController>();
         if (player == null)
@@ -13,13 +16,27 @@ public class AISensorSight : AISensorBase
 
         // Try to see if there's a direct line of sight to the player. If the hit is true and the hit target is not the player, then we quit.
         RaycastHit hit;
-        Vector3 origin = this.transform.position;
-        Vector3 direction = obj.transform.position - this.transform.position;
+        Vector3 target = obj.transform.position + new Vector3(0.0f, 3.0f, 0.0f); // This is a fucking hack that exists to overcome the fact that the character's origin is on its feet... which would make the fucking raycast hit the ground and never detect the entity...
+        Vector3 origin = this.originTransform.position;
+        Vector3 direction = (target - origin).normalized;
+
         bool hasHit = Physics.Raycast(origin, direction, out hit);
-        if (hasHit && obj != hit.collider.gameObject) // This check is kinda crap actually since it's always going to hit something... or it should at least!
+        if (!hasHit) // If for whatever reason the hit fails (which should never happen if we have detected the player...), then we bail.
             return;
-        
-        this.OnSense(obj);
-        DebugManager.Instance?.DrawLine(this.transform.position, hit.point, Color.red); // Debug sight line trace.
+
+        if (Vector3.Distance(hit.point, target) < 1.0f) // If the distance between the impact point and the target's location is small enough, we can see them.
+        {
+            // Case : Entity Detected
+            this.OnSense(obj, delta * (1.0f / distance) * this.detectionAmount);
+            debugColor = Color.green;
+        }
+        else
+        {
+            // Case : No Entity Detected
+            debugColor = Color.red;
+        }
+
+        DebugManager.Instance?.DrawLine(origin, target, debugColor); // Debug sight line trace.
+        DebugManager.Instance?.DrawSphere(hit.point, 2.0f, debugColor);
     }
 }
