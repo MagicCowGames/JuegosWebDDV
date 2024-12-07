9e354d39b60b8cc4577604ca140fb655829800a2
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-07 14:27:15 +0100

    Created WizardBehaviourController

--- UnityGame/GBS/Assets/Scripts/AISystems/Intefaces/IUtilityAction.cs
+++ UnityGame/GBS/Assets/Scripts/AISystems/Intefaces/IUtilityAction.cs
@@ -1,7 +1,8 @@
 // NOTE : This interface refers to actions for the AI Utility system.
 // TODO : Should probably implement some kind of UtilitySystem controller class of sorts that handles updating the US automatically.
 // TODO : Maybe make ISpecificUtilityAction interfaces for each type of utility and make each behaviour implement the interfaces so that we can move all of the
-// utility systems logics to be NPC Controller independent when it comes to variables such as isFleeing and stuff like that?
+// utility systems logics to be NPC Controller independent when it comes to variables such as isFleeing and stuff like that? Or just create a GenericBehaviourController
+// class which contains all of the variables we require to make the utility actions, and then inherit from that one if you want US support. Or use property setters.
 public interface IUtilityAction
 {
     public string Name { get; set; }
