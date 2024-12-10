1d8a370ca679ebbf68a6dca2601fb7d8d6808a02
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-10 21:36:11 +0100

    Added grid based random room spawning algorithm

--- UnityGame/GBS/Assets/Scripts/Controllers/Generator/DungeonGeneratorController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Generator/DungeonGeneratorController.cs
@@ -12,13 +12,43 @@ public class DungeonGeneratorController : MonoBehaviour
 
     [SerializeField] private Transform spawnTransform;
 
+    [SerializeField] private int roomsMaxX = 10;
+    [SerializeField] private int roomsMaxY = 10;
+
+    private bool[] spawnRooms;
+    private GameObject[] rooms;
+
     #endregion
 
     #region MonoBehaviour
 
     void Start()
     {
-        SpawnRandomRoom(this.smallRooms, this.spawnTransform);
+        int totalRooms = this.roomsMaxX * this.roomsMaxY;
+        this.spawnRooms = new bool[totalRooms];
+        this.rooms = new GameObject[totalRooms];
+
+        for (int i = 0; i < this.roomsMaxX; ++i)
+        {
+            for (int j = 0; j < this.roomsMaxY; ++j)
+            {
+                int globalIndex = j + i * this.roomsMaxY;
+                this.spawnRooms[globalIndex] = Random.Range(0, 2) == 1;
+            }
+        }
+
+        for (int i = 0; i < this.roomsMaxX; ++i)
+        {
+            for (int j = 0; j < this.roomsMaxY; ++j)
+            {
+                int globalIndex = j + i * this.roomsMaxY;
+                if (this.spawnRooms[globalIndex])
+                    SpawnRandomRoom(this.smallRooms, new Vector3(i * 10, 0, j * 10));
+            }
+        }
+
+        for(int i = 0; i < totalRooms; ++i)
+            Debug.Log($"room[{i}] = {spawnRooms[i]}");
     }
 
     void Update()
@@ -38,9 +68,10 @@ public class DungeonGeneratorController : MonoBehaviour
         return rooms[Random.Range(0, rooms.Length - 1)];
     }
 
-    private void SpawnRandomRoom(GameObject[] rooms, Transform spawnTransform)
+    private GameObject SpawnRandomRoom(GameObject[] rooms, Vector3 spawnPosition)
     {
-        ObjectSpawner.Spawn(GetRandomRoom(rooms), spawnTransform);
+        var obj = ObjectSpawner.Spawn(GetRandomRoom(rooms), this.spawnTransform.position + spawnPosition);
+        return obj;
     }
 
     #endregion
