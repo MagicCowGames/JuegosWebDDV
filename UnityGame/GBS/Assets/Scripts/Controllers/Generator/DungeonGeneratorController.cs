be72a3faf2d56d6ad966f6f34ddd07ac1d5520f0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-10 23:26:28 +0100

    Added grid coordinate based function to spawn random room of specified type

--- UnityGame/GBS/Assets/Scripts/Controllers/Generator/DungeonGeneratorController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Generator/DungeonGeneratorController.cs
@@ -6,12 +6,12 @@ public class DungeonGeneratorController : MonoBehaviour
 {
     #region Structs
 
-    public struct Room
+    public struct RoomPosition
     {
         int x;
         int y;
 
-        public Room(int x, int y)
+        public RoomPosition(int x, int y)
         {
             this.x = x;
             this.y = y;
@@ -66,6 +66,34 @@ public class DungeonGeneratorController : MonoBehaviour
         return obj;
     }
 
+    private RoomController SpawnRoom(RoomType type, int x, int y)
+    {
+        RoomController ans;
+        switch (type)
+        {
+            default:
+            case RoomType.Small:
+                {
+                    var obj = SpawnRandomRoom(smallRooms, new Vector3(x * 30, 0, y * 30));
+                    ans = obj.GetComponent<RoomController>();
+                }
+                break;
+            case RoomType.Medium:
+                {
+                    var obj = SpawnRandomRoom(mediumRooms, new Vector3(x * 30, 0, y * 30));
+                    ans = obj.GetComponent<RoomController>();
+                }
+                break;
+            case RoomType.Large:
+                {
+                    var obj = SpawnRandomRoom(largeRooms, new Vector3(x * 30, 0, y * 30));
+                    ans = obj.GetComponent<RoomController>();
+                }
+                break;
+        }
+        return ans;
+    }
+
     private int GetGlobalIndex(int x, int y)
     {
         if (x < 0 || y < 0 || x >= this.roomsMaxX || y >= this.roomsMaxY)
@@ -92,6 +120,7 @@ public class DungeonGeneratorController : MonoBehaviour
         }
     }
 
+    /*
     private Room RandomRoom()
     {
         int x = Random.Range(0, this.roomsMaxX);
@@ -99,6 +128,7 @@ public class DungeonGeneratorController : MonoBehaviour
         Room room = new Room(x, y);
         return room;
     }
+    */
 
     #endregion
 
@@ -210,8 +240,7 @@ public class DungeonGeneratorController : MonoBehaviour
                 int globalIndex = j + i * this.roomsMaxY;
                 if (this.spawnRooms[globalIndex])
                 {
-                    var obj = SpawnRandomRoom(this.smallRooms, new Vector3(i * 30, 0, j * 30));
-                    var room = obj.GetComponent<RoomController>();
+                    var room = SpawnRoom(RoomType.Small, i, j);
                     
                     Direction[] directions = {
                         Direction.Up,
@@ -234,6 +263,85 @@ public class DungeonGeneratorController : MonoBehaviour
 
         for (int i = 0; i < totalRooms; ++i)
             Debug.Log($"room[{i}] = {spawnRooms[i]}");
+    }
+
+    private void SpawnDungeonAlgo3()
+    {
+        int totalRooms = this.roomsMaxX * this.roomsMaxY;
+        this.spawnRooms = new bool[totalRooms];
+        this.rooms = new GameObject[totalRooms];
+
+        int randomAmountOfRooms = Random.Range(Mathf.Min(this.roomsMaxX, this.roomsMaxY), (int)Mathf.Sqrt(totalRooms));
+
+        for (int i = 0; i < randomAmountOfRooms; ++i)
+        {
+            int x = Random.Range(0, this.roomsMaxX);
+            int y = Random.Range(0, this.roomsMaxY);
+            SpawnRoom(RoomType.Small, x, y);
+            Debug.Log("fsafsafsa");
+        }
+
+        /*
+        for (int i = 0; i < this.roomsMaxX; ++i)
+        {
+            for (int j = 0; j < this.roomsMaxY; ++j)
+            {
+                int globalIndex = j + i * this.roomsMaxY;
+                this.spawnRooms[globalIndex] = Random.Range(0, 2) == 1;
+            }
+        }
+
+        for (int i = 1; i < this.roomsMaxX - 1; ++i)
+        {
+            for (int j = 1; j < this.roomsMaxY - 1; ++j)
+            {
+                int globalIndex = j + i * this.roomsMaxY;
+
+                bool u = this.spawnRooms[GetGlobalIndex(i, j + 1)];
+                bool r = this.spawnRooms[GetGlobalIndex(i + 1, j)];
+                bool d = this.spawnRooms[GetGlobalIndex(i, j - 1)];
+                bool l = this.spawnRooms[GetGlobalIndex(i - 1, j)];
+
+                // int count = u + r + d + l;
+                bool check = u && r && d && l;
+
+                if (!this.spawnRooms[globalIndex] && !check)
+                    this.spawnRooms[globalIndex] = true;
+            }
+        }
+
+        for (int i = 0; i < this.roomsMaxX; ++i)
+        {
+            for (int j = 0; j < this.roomsMaxY; ++j)
+            {
+                int globalIndex = j + i * this.roomsMaxY;
+                if (this.spawnRooms[globalIndex])
+                {
+                    var obj = SpawnRandomRoom(this.smallRooms, new Vector3(i * 30, 0, j * 30));
+                    var room = obj.GetComponent<RoomController>();
+
+                    Direction[] directions = {
+                        Direction.Up,
+                        Direction.Right,
+                        Direction.Down,
+                        Direction.Left
+                    };
+
+                    foreach (var direction in directions)
+                    {
+                        int idx = GetAdjacentRoomIndex(i, j, direction);
+                        if (idx > 0 && spawnRooms[idx])
+                        {
+                            room.RemoveWall(direction);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < totalRooms; ++i)
+            Debug.Log($"room[{i}] = {spawnRooms[i]}");
+        */
     }
 
     #endregion
