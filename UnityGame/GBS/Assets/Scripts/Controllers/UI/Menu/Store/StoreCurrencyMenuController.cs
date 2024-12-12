3ad226102c3e78ea736c5848ea837a6b265d13b9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-12 18:38:35 +0100

    Created controller scripts for item store menu and currency store menu

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Store/StoreCurrencyMenuController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoreCurrencyMenuController : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void Button_ReturnToStoreMenu()
+    {
+        SceneLoadingManager.Instance?.LoadSceneStore();
+    }
+
+    public void Button_WIP()
+    {
+        UIManager.Instance.GetPopUpUIController().Open("loc_wip_title", "loc_wip_message");
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
