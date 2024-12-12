e9ff2bfa973a693981b713c4cda1a64376bd28ab
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-12 22:18:55 +0100

    Added rotation support to room controller
    The room controller can now rotate by changing its orientation / direction.
    
    This allows rooms to appear to be less repetitive by allowing the dungeon generator to rotate rooms and face them in specific directions or randomly chosen directions.
    
    The controller handles rotations internally so that wall removal still works with the same input directions no matter the facing direction of the room.

--- UnityGame/GBS/Assets/Scripts/Controllers/Generator/RoomController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Generator/RoomController.cs
@@ -6,14 +6,30 @@ public class RoomController : MonoBehaviour
 {
     #region Variables
 
+    [SerializeField] private Transform roomRoot;
     [SerializeField] private GameObject entryUp;
     [SerializeField] private GameObject entryRight;
     [SerializeField] private GameObject entryDown;
     [SerializeField] private GameObject entryLeft;
 
+    private GameObject originalEntryUp;
+    private GameObject originalEntryRight;
+    private GameObject originalEntryDown;
+    private GameObject originalEntryLeft;
+
+    private Direction faceDirection = Direction.Up;
+
     #endregion
 
     #region MonoBehaviour
+
+    void Awake()
+    {
+        this.originalEntryUp = this.entryUp;
+        this.originalEntryRight = this.entryRight;
+        this.originalEntryDown = this.entryDown;
+        this.originalEntryLeft = this.entryLeft;
+    }
 
     void Start()
     {
@@ -58,8 +74,65 @@ public class RoomController : MonoBehaviour
         SetWallEnabled(direction, true);
     }
 
+    public void SetFaceDirection(Direction direction)
+    {
+        this.faceDirection = direction;
+
+        ResetRotation();
+
+        int timesToRotate = 0;
+
+        switch (direction)
+        {
+            default:
+            case Direction.Up:
+                // Do nothing, we're already facing up after the reset rotation call
+                timesToRotate = 0;
+                break;
+            case Direction.Right:
+                timesToRotate = 1;
+                break;
+            case Direction.Down:
+                timesToRotate = 2;
+                break;
+            case Direction.Left:
+                timesToRotate = 3;
+                break;
+        }
+
+        Rotate90DegreesRight(timesToRotate);
+    }
+
     #endregion
 
     #region PrivateMethods
+
+    private void Rotate90DegreesRight(int times = 1)
+    {
+        for (int i = 0; i < times; ++i)
+        {
+            this.roomRoot.transform.Rotate(new Vector3(0.0f, 90.0f, 0.0f));
+
+            var u = this.entryUp;
+            var r = this.entryRight;
+            var d = this.entryDown;
+            var l = this.entryLeft;
+
+            this.entryUp = l;
+            this.entryRight = u;
+            this.entryDown = r;
+            this.entryLeft = d;
+        }
+    }
+
+    private void ResetRotation()
+    {
+        this.roomRoot.rotation = Quaternion.identity;
+        this.entryUp = this.originalEntryUp;
+        this.entryRight = this.originalEntryRight;
+        this.entryDown = this.originalEntryDown;
+        this.entryLeft = this.originalEntryLeft;
+    }
+
     #endregion
 }
