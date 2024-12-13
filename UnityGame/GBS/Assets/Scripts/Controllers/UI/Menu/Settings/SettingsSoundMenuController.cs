06fe5cec08c5b1a37de27d48936bf9f4f030f7fa
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-13 20:52:17 +0100

    Implemented functionality to handle new volume bars in sound settings menu controller

--- UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsSoundMenuController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/UI/Menu/Settings/SettingsSoundMenuController.cs
@@ -7,8 +7,11 @@ public class SettingsSoundMenuController : MonoBehaviour
 {
     #region Variables
 
-    // [SerializeField] private Image volumeImageMusic;
+    [SerializeField] private Slider volumeSliderGlobal;
+    [SerializeField] private Slider volumeSliderSFX;
     [SerializeField] private Slider volumeSliderMusic;
+    [SerializeField] private Slider volumeSliderVoice;
+    [SerializeField] private Slider volumeSliderUI;
 
     #endregion
 
@@ -16,18 +19,16 @@ public class SettingsSoundMenuController : MonoBehaviour
 
     void Start()
     {
+        this.volumeSliderGlobal.value = SoundManager.Instance.VolumeGlobal;
+        this.volumeSliderSFX.value = SoundManager.Instance.VolumeSFX;
         this.volumeSliderMusic.value = SoundManager.Instance.VolumeMusic;
+        this.volumeSliderVoice.value = SoundManager.Instance.VolumeVoice;
+        this.volumeSliderUI.value = SoundManager.Instance.VolumeUI;
     }
 
     void Update()
     {
-        /*
-        float delta = Time.deltaTime;
-        float speed = 10.0f;
-        float sdt = delta * speed;
 
-        this.volumeImageMusic.fillAmount = Mathf.Lerp(this.volumeImageMusic.fillAmount, SoundManager.Instance.GetMusicVolume(), sdt);
-        */
     }
 
     #endregion
@@ -47,9 +48,29 @@ public class SettingsSoundMenuController : MonoBehaviour
     }
     */
 
+    public void Slider_SetVolume_Global(float volume)
+    {
+        SoundManager.Instance.VolumeGlobal = volume;
+    }
+
+    public void Slider_SetVolume_SFX(float volume)
+    {
+        SoundManager.Instance.VolumeSFX = volume;
+    }
+
     public void Slider_SetVolume_Music(float volume)
     {
         SoundManager.Instance.VolumeMusic = volume;
+    }
+
+    public void Slider_SetVolume_Voice(float volume)
+    {
+        SoundManager.Instance.VolumeVoice = volume;
+    }
+
+    public void Slider_SetVolume_UI(float volume)
+    {
+        SoundManager.Instance.VolumeUI = volume;
     }
 
     public void Button_Return()
