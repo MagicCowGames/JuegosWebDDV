4ed00fd578200badacd36ea1ab793e21628145bc
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-14 17:37:39 +0100

    Added UnityEvents on dialogue start and end

--- UnityGame/GBS/Assets/Scripts/Data/Dialogue.cs
+++ UnityGame/GBS/Assets/Scripts/Data/Dialogue.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [System.Serializable]
 public struct Dialogue
@@ -6,12 +7,16 @@ public struct Dialogue
     public Sprite characterSprite;
     public string name;
     public string text;
+    public UnityEvent onDialogueStart;
+    public UnityEvent onDialogueEnd;
 
-    public Dialogue(Sprite characterSprite, string name, string text)
+    public Dialogue(Sprite characterSprite, string name, string text, UnityEvent onDialogueStart, UnityEvent onDialogueEnd)
     {
         this.characterSprite = characterSprite;
         this.name = name;
         this.text = text;
+        this.onDialogueStart = onDialogueStart;
+        this.onDialogueEnd = onDialogueEnd;
     }
 }
 
