a3f61dd64fc33a42bbf6e19bfd6735c43f1a549c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-20 21:26:07 +0100

    Created DebugSceneController
    This scene controller allows running a simple test on the debug object pool scene to test the correct functionality of object pools.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/Scenes/Debug/DebugSceneController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugSceneController : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private ObjectPool objectPool;
+    [SerializeField] private float timeToDespawn = 2.0f;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void SpawnThing()
+    {
+        var obj = this.objectPool.Get();
+        if (obj != null)
+            StartCoroutine(DespawnThingCoroutine(obj));
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private IEnumerator DespawnThingCoroutine(GameObject obj)
+    {
+        yield return new WaitForSeconds(this.timeToDespawn);
+        this.objectPool.Return(obj);
+    }
+
+    #endregion
+}
