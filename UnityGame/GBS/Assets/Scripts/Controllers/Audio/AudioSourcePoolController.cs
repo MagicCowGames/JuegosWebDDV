95de1043672dd06480c859d467d2f1cff533eeba
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-20 18:00:53 +0100

    Implemented basic sound pool controller logic

--- UnityGame/GBS/Assets/Scripts/Controllers/Audio/AudioSourcePoolController.cs
+++ UnityGame/GBS/Assets/Scripts/Controllers/Audio/AudioSourcePoolController.cs
@@ -6,13 +6,23 @@ public class AudioSourcePoolController : MonoBehaviour
 {
     #region Variables
 
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int startingCount = 10;
+    [SerializeField] private int maxCount = 100;
+
+    private List<AudioSource> audioSources;
+
     #endregion
 
     #region MonoBehaviour
 
     void Start()
     {
-        
+        this.audioSources = new List<AudioSource>();
+        for (int i = 0; i < startingCount; ++i)
+        {
+            SpawnAudioSource();
+        }
     }
 
     void Update()
@@ -23,8 +33,49 @@ public class AudioSourcePoolController : MonoBehaviour
     #endregion
 
     #region PublicMethods
+
+    // TODO : Maybe implement some kind of SoundSettings class or struct and use that as a parameter to contain the requested clip, volume multiplier, pitch, etc...
+    public void PlaySound(AudioClip clip)
+    {
+        // TODO : Implement
+    }
+
     #endregion
 
     #region PrivateMethods
+
+    private AudioSource SpawnAudioSource()
+    {
+        var obj = ObjectSpawner.Spawn(prefab, this.transform);
+        obj.transform.parent = this.transform;
+        var audioSource = obj.GetComponent<AudioSource>();
+        this.audioSources.Add(audioSource);
+        audioSource.gameObject.SetActive(false);
+        return audioSource;
+    }
+
+    // TODO : Refactor to push the inactive audio sources to the end of the list so that we can just use an "activeCount" to instantly get the correct index.
+    private AudioSource GetAudioSource()
+    {
+        foreach (var source in this.audioSources)
+        {
+            if (!source.gameObject.activeSelf)
+            {
+                source.gameObject.SetActive(true);
+                return source;
+            }
+        }
+
+        if (this.audioSources.Count < this.maxCount)
+            return SpawnAudioSource();
+
+        return null;
+    }
+
+    private void ReturnAudioSource(AudioSource source)
+    {
+        source.gameObject.SetActive(false);
+    }
+
     #endregion
 }
