755736e9637d78d9f63a1eb8d70918934f0e1126
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-20 19:29:37 +0100

    Removed outdated code

--- UnityGame/GBS/Assets/Scripts/Utility/ObjectPool/ObjectPool.cs
+++ UnityGame/GBS/Assets/Scripts/Utility/ObjectPool/ObjectPool.cs
@@ -43,44 +43,8 @@ public class ObjectPool : MonoBehaviour
     #endregion
 
     #region PublicMethods
-    /* TODO : Implement
-    public GameObject Get()
-    {
-        GameObject ans = null;
-        if (this.Count < this.Capacity)
-        {
-            GameObject obj = this.Objects[this.Count];
-            obj.SetActive(true);
-            ++this.Count;
-        }
-        else
-        if (this.Count < this.MaxCapacity)
-        {
-            var obj = ObjectSpawner.Spawn(this.Prefab);
-            obj.SetActive(true);
-            this.Objects.Add(obj);
-            ++this.Count;
-        }
-        return ans;
-    }
 
-    // TODO : Replace the GameObject class with a custom PooleableGameObject class or something like that, and make it so that that class just inherits from the base
-    // GameObject class and maybe adds some pool-local ID integer value so that the Return() function can be O(1)
-    public void Return(GameObject obj)
-    {
-        for (int i = 0; i < this.Count; ++i)
-        {
-            if (this.Objects[i] == obj)
-            {
-                var temp = this.Objects[this.Count - 1];
-                this.Objects[this.Count - 1] = this.Objects[i];
-                this.Objects[i] = temp;
-                this.Objects[i].SetActive(false);
-                --this.Count;
-            }
-        }
-    }
-    */
+    
 
     #endregion
 
