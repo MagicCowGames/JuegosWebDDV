a35e18c47c8e061f5855e56e05ebb655ca355b79
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-20 17:35:49 +0100

    Created AudioSourcePoolController class
    The AudioSourcePoolController class will be in charge of controlling and managing the pooling of a specific type of audio sources with an audio source prefab of the user's choosing.
    
    This way, we will be able to standardise the audio playing system even further and simplifying the logic by moving a lot of the duplicated specific logic from the SoundManager to this class and making the SoundManager manager sound events through Audio source pools.

---
+++ UnityGame/GBS/Assets/Scripts/Controllers/Audio/AudioSourcePoolController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourcePoolController : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+    #endregion
+}
