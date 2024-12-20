3db0eceb838b915fcd6aeeb8f3577c6696a09caf
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-20 19:27:16 +0100

    Implemented basic object pool object spawning and public serialized properties

--- UnityGame/GBS/Assets/Scripts/Utility/ObjectPool/ObjectPool.cs
+++ UnityGame/GBS/Assets/Scripts/Utility/ObjectPool/ObjectPool.cs
@@ -2,39 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Object Pool implementation with dynamic reallocation / capcity growth
-public class ObjectPool
+// NOTE : This ObjectPool implementation uses dynamic reallocation with capcity growth up to a maximum limit.
+public class ObjectPool : MonoBehaviour
 {
     #region Variables
 
-    public GameObject Prefab { get; private set; }
-    public List<GameObject> Objects { get; private set; }
-    public int Capacity { get; private set; }
-    public int MaxCapacity { get; private set; }
-    public int Count { get; private set; }
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int initialCount;
+    [SerializeField] private int maxCount;
+
+    private int activeCount;
+
+    private List<GameObject> objects;
 
     #endregion
 
-    #region Constructor
+    #region Properties
 
-    public ObjectPool(GameObject prefab, int initialCapacity, int maxCapacity)
+    // Prefab
+    public GameObject Prefab { get { return this.prefab; } set { this.prefab = value; } }
+
+    // Counts
+    public int InitialCount { get { return this.initialCount; } set { this.initialCount = value; } }
+    public int MaxCount { get { return this.maxCount; } set { this.maxCount = value; } }
+    public int ActiveCount { get { return this.activeCount; } }
+    public int TotalCount { get { return this.objects.Count; } }
+
+    // GameObjects list
+    public List<GameObject> Objects { get { return this.Objects; } }
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Awake()
     {
-        this.Count = 0;
-        this.Prefab = prefab;
-        this.Capacity = initialCapacity;
-        this.MaxCapacity = maxCapacity;
-        this.Objects = new List<GameObject>(this.Capacity);
-        for (int i = 0; i < this.Capacity; ++i)
-        {
-            this.Objects[i] = ObjectSpawner.Spawn(this.Prefab);
-            this.Objects[i].SetActive(false);
-        }
+        Init();
     }
 
     #endregion
 
     #region PublicMethods
-
+    /* TODO : Implement
     public GameObject Get()
     {
         GameObject ans = null;
@@ -71,9 +80,36 @@ public class ObjectPool
             }
         }
     }
+    */
 
     #endregion
 
     #region PrivateMethods
+
+    private void Init()
+    {
+        this.activeCount = 0;
+        this.objects = new List<GameObject>(this.initialCount);
+        for (int i = 0; i < this.initialCount; ++i)
+            this.Objects[i] = SpawnObject();
+    }
+
+    private GameObject SpawnObject()
+    {
+        // Spawn the object
+        var obj = ObjectSpawner.Spawn(this.prefab, this.transform);
+        obj.transform.parent = this.transform;
+
+        // Add pooleable object controller component
+        var pooleable = obj.AddComponent<PooleableObjectController>();
+        pooleable.Index = this.objects.Count;
+
+        // Add object to list
+        this.objects.Add(obj); // NOTE : The total count value is increased when adding the object to the objects list.
+
+        // Return the spawned object
+        return obj;
+    }
+
     #endregion
 }
