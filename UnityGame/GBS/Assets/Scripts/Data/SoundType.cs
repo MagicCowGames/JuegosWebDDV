f89b4ae961b1a4693ddeac4b3864388b88180110
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-12-22 19:26:12 +0100

    Added new sound type enum and created SoundData struct

--- UnityGame/GBS/Assets/Scripts/Data/SoundType.cs
+++ UnityGame/GBS/Assets/Scripts/Data/SoundType.cs
@@ -1,8 +1,32 @@
+using UnityEngine;
+
 public enum SoundType
 {
-    None = 0,
-    Ambient,
-    Voice,
+    SFX = 0,
     Music,
+    Voice,
     UI
+}
+
+public enum SoundPlayType
+{
+    Global = 0,
+    Local
+}
+
+[System.Serializable]
+public struct SoundData
+{
+    public AudioClip clip;
+    public bool loop;
+    public float volume;
+    public float pitch;
+
+    public SoundData(AudioClip clip, bool loop = false, float volume = 1.0f, float pitch = 1.0f)
+    {
+        this.clip = clip;
+        this.loop = loop;
+        this.volume = volume;
+        this.pitch = pitch;
+    }
 }
